4258843393527ecf868f59821f11b783a335151a
Gugu42 <gugo42800@gmail.com>
2019-02-02 13:16:00 +0100

    Craft load PoC

---
+++ AAEmu.Game/Models/Game/Crafts/Craft.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AAEmu.Game.Models.Game.Crafts
+{
+    /*
+        Data relating to a craft.
+    */
+    public class Craft
+    {
+        public uint Id {get; set;}
+        public int CastDelay {get; set;}
+        public int ToolId {get; set;}
+        public int SkillId {get; set;}
+        public int WiId {get; set;}
+        public int MilestoneId {get; set;}
+        public int ReqDoodadId {get; set;}
+        public bool NeedBind {get; set;}
+        public int AcId {get; set;}
+        public int ActabilityLimit {get; set;}
+        public bool ShowUpperCraft {get; set;}
+        public int RecommendLevel {get; set;}
+        public int VisibleOrder {get; set;}
+    }
+}
