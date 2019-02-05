6a5fa7d838761bbc25f5ac43e1483a5f99e1f139
Gugu42 <gugo42800@gmail.com>
2019-02-05 11:22:31 +0100

    [Game] -update: DoodadFuncTimer calls next phase, LootItem gives items

--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncLootItem.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncLootItem.cs
@@ -1,4 +1,9 @@
+using AAEmu.Commons.Utils;
+using AAEmu.Game.Core.Helper;
+using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.DoodadObj.Templates;
+using AAEmu.Game.Models.Game.Items;
 using AAEmu.Game.Models.Game.Units;
 
 namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
@@ -15,6 +20,17 @@ namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
         
         public override void Use(Unit caster, Doodad owner, uint skillId)
         {
+            Character character = (Character) caster;
+            if (character == null) return;
+            
+            int chance = Rand.Next(0, 10000);
+            if (chance > Percent) return;
+
+            int count = Rand.Next(CountMin, CountMax);
+            
+            Item item = ItemManager.Instance.Create(ItemId, count, 0);
+            InventoryHelper.AddItemAndUpdateClient(character, item);
+            
             _log.Debug("DoodadFuncLootItem");
         }
     }
--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncTimer.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncTimer.cs
@@ -1,5 +1,6 @@
 using System;
 using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Core.Managers.UnitManagers;
 using AAEmu.Game.Models.Game.DoodadObj.Templates;
 using AAEmu.Game.Models.Game.Units;
 using AAEmu.Game.Models.Tasks.Doodads;
@@ -17,7 +18,11 @@ namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
 
         public override void Use(Unit caster, Doodad owner, uint skillId)
         {
-            _log.Debug("DoodadFuncTimer");
+            _log.Debug("DoodadFuncTimer : NextPhase {0}, SkillId {1}", NextPhase, skillId);
+
+            //This is a temporary fix. We need to find how to properly call the next function.
+            var nextFunc = DoodadManager.Instance.GetFunc(owner.FuncGroupId, skillId);
+            if (nextFunc != null) nextFunc.Use(caster, owner, skillId);
 
             if (Delay > 0)
             {
