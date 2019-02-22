20c9153df4edb97109a0d31b66b3a4537e6d9e0a
Mr. Nikes <balashovalex858@gmail.com>
2019-02-22 11:31:53 +0200

    [Login] -fix: login by trion provider

--- AAEmu.Login/Core/Packets/C2L/CARequestAuthTrionPacket.cs
+++ AAEmu.Login/Core/Packets/C2L/CARequestAuthTrionPacket.cs
@@ -1,4 +1,7 @@
+using System.Xml.Linq;
 using AAEmu.Commons.Network;
+using AAEmu.Commons.Utils;
+using AAEmu.Login.Core.Controllers;
 using AAEmu.Login.Core.Network.Login;
 
 namespace AAEmu.Login.Core.Packets.C2L
@@ -18,8 +21,26 @@ namespace AAEmu.Login.Core.Packets.C2L
             var ticket = stream.ReadString();
             var signature = stream.ReadString();
             var isLast = stream.ReadBoolean();
-            
-            _log.Warn("RequestAuthTrion");
+
+            var xmlDoc = XDocument.Parse(ticket);
+
+            if (xmlDoc.Root == null)
+            {
+                _log.Error("RequestAuthTrion: Catch parse ticket");
+                return;
+            }
+
+            var username = xmlDoc.Root.Element("username")?.Value;
+            var password = xmlDoc.Root.Element("password")?.Value;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                _log.Error("RequestAuthTrion: username or password is empty or white space");
+                return;
+            }
+
+            var token = Helpers.StringToByteArray(password);
+            LoginController.Login(Connection, username, token);
         }
     }
 }
