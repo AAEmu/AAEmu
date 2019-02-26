6290597a78350e5dc83bd750e296c513631e478a
nikes <balashovalex858@gmail.com>
2019-02-26 20:36:51 +0200

    [SQL] -fix: lost worldId

--- SQL/aaemu_game.sql
+++ SQL/aaemu_game.sql
@@ -1,61 +1,71 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 26 2019 г., 21:35
+-- Версия сервера: 8.0.12
+-- Версия PHP: 7.2.10
 
- Source Server         : aaemu
- Source Server Type    : MySQL
- Source Server Version : 80015
- Source Host           : localhost:3306
- Source Schema         : aaemu_game
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 80015
- File Encoding         : 65001
 
- Date: 18/02/2019 11:30:59
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- База данных: `aaemu_game`
+--
 
--- ----------------------------
--- Table structure for abilities
--- ----------------------------
-DROP TABLE IF EXISTS `abilities`;
-CREATE TABLE `abilities`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `abilities`
+--
+
+CREATE TABLE `abilities` (
   `id` tinyint(3) UNSIGNED NOT NULL,
   `exp` int(11) NOT NULL,
-  `owner` int(11) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `owner` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for actabilities
--- ----------------------------
-DROP TABLE IF EXISTS `actabilities`;
-CREATE TABLE `actabilities`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `actabilities`
+--
+
+CREATE TABLE `actabilities` (
   `id` int(10) UNSIGNED NOT NULL,
-  `point` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `step` tinyint(3) UNSIGNED NOT NULL DEFAULT 0,
-  `owner` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`owner`, `id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `point` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `step` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
+  `owner` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for appellations
--- ----------------------------
-DROP TABLE IF EXISTS `appellations`;
-CREATE TABLE `appellations`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `appellations`
+--
+
+CREATE TABLE `appellations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT 0,
-  `owner` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `owner` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for characters
--- ----------------------------
-DROP TABLE IF EXISTS `characters`;
-CREATE TABLE `characters`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `characters`
+--
+
+CREATE TABLE `characters` (
   `id` int(11) UNSIGNED NOT NULL,
   `account_id` int(11) UNSIGNED NOT NULL,
   `name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -68,11 +78,12 @@ CREATE TABLE `characters`  (
   `hp` int(11) NOT NULL,
   `mp` int(11) NOT NULL,
   `labor_power` mediumint(9) NOT NULL,
-  `labor_power_modified` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `labor_power_modified` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
   `consumed_lp` int(11) NOT NULL,
   `ability1` tinyint(4) NOT NULL,
   `ability2` tinyint(4) NOT NULL,
   `ability3` tinyint(4) NOT NULL,
+  `world_id` int(11) UNSIGNED NOT NULL,
   `zone_id` int(11) UNSIGNED NOT NULL,
   `x` float NOT NULL,
   `y` float NOT NULL,
@@ -84,129 +95,208 @@ CREATE TABLE `characters`  (
   `faction_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `family` int(11) UNSIGNED NOT NULL,
   `dead_count` mediumint(8) UNSIGNED NOT NULL,
-  `dead_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `dead_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
   `rez_wait_duration` int(11) NOT NULL,
-  `rez_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `rez_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
   `rez_penalty_duration` int(11) NOT NULL,
-  `leave_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `leave_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
   `money` bigint(20) NOT NULL,
   `money2` bigint(20) NOT NULL,
   `honor_point` int(11) NOT NULL,
   `vocation_point` int(11) NOT NULL,
   `crime_point` int(11) NOT NULL,
   `crime_record` int(11) NOT NULL,
-  `delete_request_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
-  `transfer_request_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
-  `delete_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `delete_request_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `transfer_request_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `delete_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
   `bm_point` int(11) NOT NULL,
   `auto_use_aapoint` tinyint(1) NOT NULL,
   `prev_point` int(11) NOT NULL,
   `point` int(11) NOT NULL,
   `gift` int(11) NOT NULL,
-  `num_inv_slot` tinyint(3) UNSIGNED NOT NULL DEFAULT 50,
-  `num_bank_slot` smallint(5) UNSIGNED NOT NULL DEFAULT 50,
+  `num_inv_slot` tinyint(3) UNSIGNED NOT NULL DEFAULT '50',
+  `num_bank_slot` smallint(5) UNSIGNED NOT NULL DEFAULT '50',
   `expanded_expert` tinyint(4) NOT NULL,
   `slots` blob NOT NULL,
-  `created_at` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `updated_at` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
-  PRIMARY KEY (`id`, `account_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT '0001-01-01 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for family_members
--- ----------------------------
-DROP TABLE IF EXISTS `family_members`;
-CREATE TABLE `family_members`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `family_members`
+--
+
+CREATE TABLE `family_members` (
   `character_id` int(11) NOT NULL,
   `family_id` int(11) NOT NULL,
   `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `role` tinyint(1) NOT NULL DEFAULT 0,
-  `title` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`family_id`, `character_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `role` tinyint(1) NOT NULL DEFAULT '0',
+  `title` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for friends
--- ----------------------------
-DROP TABLE IF EXISTS `friends`;
-CREATE TABLE `friends`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `friends`
+--
+
+CREATE TABLE `friends` (
   `id` int(11) NOT NULL,
   `friend_id` int(11) NOT NULL,
-  `owner` int(11) NOT NULL,
-  PRIMARY KEY (`id`, `owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for items
--- ----------------------------
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE `items`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `items`
+--
+
+CREATE TABLE `items` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `template_id` int(11) UNSIGNED NOT NULL,
   `slot_type` enum('Equipment','Inventory','Bank') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `slot` int(11) NOT NULL,
   `count` int(11) NOT NULL,
-  `details` blob NULL,
+  `details` blob,
   `lifespan_mins` int(11) NOT NULL,
-  `unsecure_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
-  `unpack_time` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `unsecure_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
+  `unpack_time` datetime NOT NULL DEFAULT '0001-01-01 00:00:00',
   `owner` int(11) UNSIGNED NOT NULL,
-  `grade` tinyint(1) NULL DEFAULT 0,
-  `created_at` datetime(0) NOT NULL DEFAULT '0001-01-01 00:00:00',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `owner`(`owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `grade` tinyint(1) DEFAULT '0',
+  `created_at` datetime NOT NULL DEFAULT '0001-01-01 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for options
--- ----------------------------
-DROP TABLE IF EXISTS `options`;
-CREATE TABLE `options`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `options`
+--
+
+CREATE TABLE `options` (
   `key` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `value` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `owner` int(11) UNSIGNED NOT NULL,
-  PRIMARY KEY (`key`, `owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `owner` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for portal_book_coords
--- ----------------------------
-DROP TABLE IF EXISTS `portal_book_coords`;
-CREATE TABLE `portal_book_coords`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `portal_book_coords`
+--
+
+CREATE TABLE `portal_book_coords` (
   `id` int(11) NOT NULL,
   `name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `x` int(11) NULL DEFAULT 0,
-  `y` int(11) NULL DEFAULT 0,
-  `z` int(11) NULL DEFAULT 0,
-  `zone_id` int(11) NULL DEFAULT 0,
-  `z_rot` int(11) NULL DEFAULT 0,
-  `sub_zone_id` int(11) NULL DEFAULT 0,
-  `owner` int(11) NOT NULL,
-  PRIMARY KEY (`id`, `owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `x` int(11) DEFAULT '0',
+  `y` int(11) DEFAULT '0',
+  `z` int(11) DEFAULT '0',
+  `zone_id` int(11) DEFAULT '0',
+  `z_rot` int(11) DEFAULT '0',
+  `sub_zone_id` int(11) DEFAULT '0',
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for portal_visited_district
--- ----------------------------
-DROP TABLE IF EXISTS `portal_visited_district`;
-CREATE TABLE `portal_visited_district`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `portal_visited_district`
+--
+
+CREATE TABLE `portal_visited_district` (
   `id` int(11) NOT NULL,
   `subzone` int(11) NOT NULL,
-  `owner` int(11) NOT NULL,
-  PRIMARY KEY (`id`, `subzone`, `owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for skills
--- ----------------------------
-DROP TABLE IF EXISTS `skills`;
-CREATE TABLE `skills`  (
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `skills`
+--
+
+CREATE TABLE `skills` (
   `id` int(11) UNSIGNED NOT NULL,
   `level` tinyint(4) NOT NULL,
   `type` enum('Skill','Buff') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `owner` int(11) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `owner`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `owner` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-SET FOREIGN_KEY_CHECKS = 1;
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `abilities`
+--
+ALTER TABLE `abilities`
+  ADD PRIMARY KEY (`id`,`owner`) USING BTREE;
+
+--
+-- Индексы таблицы `actabilities`
+--
+ALTER TABLE `actabilities`
+  ADD PRIMARY KEY (`owner`,`id`) USING BTREE;
+
+--
+-- Индексы таблицы `appellations`
+--
+ALTER TABLE `appellations`
+  ADD PRIMARY KEY (`id`,`owner`) USING BTREE;
+
+--
+-- Индексы таблицы `characters`
+--
+ALTER TABLE `characters`
+  ADD PRIMARY KEY (`id`,`account_id`) USING BTREE;
+
+--
+-- Индексы таблицы `family_members`
+--
+ALTER TABLE `family_members`
+  ADD PRIMARY KEY (`family_id`,`character_id`) USING BTREE;
+
+--
+-- Индексы таблицы `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`id`,`owner`) USING BTREE;
+
+--
+-- Индексы таблицы `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `owner` (`owner`) USING BTREE;
+
+--
+-- Индексы таблицы `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`key`,`owner`) USING BTREE;
+
+--
+-- Индексы таблицы `portal_book_coords`
+--
+ALTER TABLE `portal_book_coords`
+  ADD PRIMARY KEY (`id`,`owner`) USING BTREE;
+
+--
+-- Индексы таблицы `portal_visited_district`
+--
+ALTER TABLE `portal_visited_district`
+  ADD PRIMARY KEY (`id`,`subzone`,`owner`) USING BTREE;
+
+--
+-- Индексы таблицы `skills`
+--
+ALTER TABLE `skills`
+  ADD PRIMARY KEY (`id`,`owner`) USING BTREE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
