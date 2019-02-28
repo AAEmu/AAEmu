3d316425a8d456489d2a2b9b9f125bb652cd4424
Lemes <gustavolemes_94@hotmail.com>
2019-02-28 17:55:18 -0300

    [Game] -add mate -add shipyard basic

--- AAEmu.Game/Core/Packets/C2G/CSChangeMateNamePacket.cs
+++ AAEmu.Game/Core/Packets/C2G/CSChangeMateNamePacket.cs
@@ -1,4 +1,4 @@
-using AAEmu.Commons.Network;
+﻿using AAEmu.Commons.Network;
 using AAEmu.Game.Core.Network.Game;
 
 namespace AAEmu.Game.Core.Packets.C2G
@@ -11,10 +11,11 @@ namespace AAEmu.Game.Core.Packets.C2G
 
         public override void Read(PacketStream stream)
         {
-            var tl = stream.ReadUInt16();
+            var tlId = stream.ReadUInt16();
             var name = stream.ReadString();
 
-            _log.Warn("ChangeMateName, TlId: {0}, Name: {1}", tl, name);
+            //_log.Warn("ChangeMateName, TlId: {0}, Name: {1}", tlId, name);
+            Connection.ActiveChar.Mates.RenameMate(tlId, name);
         }
     }
 }
---
+++ AAEmu.Game/Models/Game/Shipyard/Shipyard.cs
@@ -0,0 +1,45 @@
+﻿using AAEmu.Game.Core.Packets.G2C;
+using AAEmu.Game.Models.Game.Char;
+using AAEmu.Game.Models.Game.Housing;
+using AAEmu.Game.Models.Game.Units;
+using NLog;
+
+namespace AAEmu.Game.Models.Game.Shipyard
+{
+    public sealed class Shipyard : Unit
+    {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+
+        public uint Id { get; set; }
+        public ushort TlId { get; set; }
+        public uint TemplateId { get; set; }
+        public ShipyardData Template { get; set; }
+        public int Step { get; set; }
+        public override int MaxHp { get; set; }
+        public override UnitCustomModelParams ModelParams { get; set; }
+
+        public Shipyard()
+        {
+            ModelParams = new UnitCustomModelParams();
+        }
+
+        public override void AddVisibleObject(Character character)
+        {
+            character.SendPacket(new SCUnitStatePacket(this));
+            character.SendPacket(new SCShipyardStatePacket(Template, 1));
+            //character.SendPacket(new SCUnitPointsPacket(ObjId, Hp, Mp));
+        }
+
+        public override void RemoveVisibleObject(Character character)
+        {
+            if (character.CurrentTarget != null && character.CurrentTarget == this)
+            {
+                character.CurrentTarget = null;
+                character.SendPacket(new SCTargetChangedPacket(character.ObjId, 0));
+            }
+
+            character.SendPacket(new SCUnitsRemovedPacket(new[] { ObjId }));
+
+        }
+    }
+}
