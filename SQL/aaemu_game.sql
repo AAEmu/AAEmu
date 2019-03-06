ae7e25d0d028564fdf35e0db86287abba015f46e
nikes <balashovalex858@gmail.com>
2019-03-06 23:17:35 +0200

    [SQL] -add: housing sql

--- SQL/aaemu_game.sql
+++ SQL/aaemu_game.sql
@@ -129,6 +129,20 @@ CREATE TABLE IF NOT EXISTS `friends` (
   PRIMARY KEY (`id`,`owner`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
+CREATE TABLE IF NOT EXISTS `housings` (
+  `id` int(11) NOT NULL,
+  `account_id` int(10) UNSIGNED NOT NULL,
+  `owner` int(10) UNSIGNED NOT NULL,
+  `template_id` int(10) UNSIGNED NOT NULL,
+  `x` float NOT NULL,
+  `y` float NOT NULL,
+  `z` float NOT NULL,
+  `rotation_z` tinyint(4) NOT NULL,
+  `current_step` tinyint(4) NOT NULL,
+  `permission` tinyint(4) NOT NULL,
+  PRIMARY KEY (`account_id`,`owner`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `items` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
