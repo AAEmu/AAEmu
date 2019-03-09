ac2046188b1155d5d48bd24668ce41ee0ff00480
Lemes <gustavolemes_94@hotmail.com>
2019-03-09 14:07:27 -0300

    [Game] -slave doodads

--- AAEmu.Game/Core/Packets/C2G/CSDiscardSlavePacket.cs
+++ AAEmu.Game/Core/Packets/C2G/CSDiscardSlavePacket.cs
@@ -1,4 +1,5 @@
 using AAEmu.Commons.Network;
+using AAEmu.Game.Core.Managers;
 using AAEmu.Game.Core.Network.Game;
 
 namespace AAEmu.Game.Core.Packets.C2G
@@ -11,9 +12,10 @@ namespace AAEmu.Game.Core.Packets.C2G
 
         public override void Read(PacketStream stream)
         {
-            var tl = stream.ReadUInt16();
-            
-            _log.Debug("DiscardSlave, Tl: {0}", tl);
+            var tlId = stream.ReadUInt16();
+
+            //_log.Debug("DiscardSlave, Tl: {0}", tlId);
+            SlaveManager.Instance.UnbindSlave(Connection, tlId);
         }
     }
 }
