f9aeafd1ab46db2f97ae3d4e345d9a42fde07205
Razy <amor.easa@gmail.com>
2019-04-19 13:38:02 +0200

    Missing Packets (#68)
    [Game] -adding some CS missing packets

---
+++ AAEmu.Game/Core/Packets/C2G/CSRankCharacterPacket.cs
@@ -0,0 +1,18 @@
+﻿using AAEmu.Commons.Network;
+using AAEmu.Game.Core.Network.Game;
+
+namespace AAEmu.Game.Core.Packets.C2G
+{
+    public class CSRankCharacterPacket : GamePacket
+    {
+        public CSRankCharacterPacket() : base(0x12F, 1)
+        {
+        }
+
+        public override void Read(PacketStream stream)
+        {
+            // Empty struct
+            _log.Debug("RankCharacter");
+        }
+    }
+}
