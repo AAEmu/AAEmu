97077d0bfc110ef26a8229bfbfa2d0d47a28b2b8
Yanlong-li <ahlyl94@gmail.com>
2019-04-21 00:27:10 +0800

    Revert "增加npc移动脚本，当前处于开发阶段，可进行圆形走动（可设定其他路线）。"
    This reverts commit df4fb811a53202d5d260afba55c1ddfd299fa540.

--- AAEmu.Game/Core/Packets/C2G/CSChangeTargetPacket.cs
+++ AAEmu.Game/Core/Packets/C2G/CSChangeTargetPacket.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using AAEmu.Commons.Network;
-using AAEmu.Game.Core.Managers;
+﻿using AAEmu.Commons.Network;
 using AAEmu.Game.Core.Managers.World;
 using AAEmu.Game.Core.Network.Game;
 using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.NPChar;
-using AAEmu.Game.Models.Game.Units.Movements;
-using AAEmu.Game.Models.Tasks.UnitMove;
 
 namespace AAEmu.Game.Core.Packets.C2G
 {
@@ -32,16 +28,10 @@ namespace AAEmu.Game.Core.Packets.C2G
 
             if (Connection.ActiveChar.CurrentTarget == null)
                 return;
-            if (Connection.ActiveChar.CurrentTarget is Npc npc) { 
+            if (Connection.ActiveChar.CurrentTarget is Npc npc)
                 Connection.ActiveChar.SendMessage("ObjId: {0}, TemplateId: {1}", targetId, npc.TemplateId);
-
-                TaskManager.Instance.Schedule(
-                    new UnitMove(new Patrol(),Connection.ActiveChar, npc), TimeSpan.FromMilliseconds(365)
-                 );
-            }
             else if (Connection.ActiveChar.CurrentTarget is Character character)
                 Connection.ActiveChar.SendMessage("ObjId: {0}, CharacterId: {1}", targetId, character.Id);
-
         }
     }
 }
