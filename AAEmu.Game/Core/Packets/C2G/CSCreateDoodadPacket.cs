62603b63baf831433b1dad60222fb4d3d1ae18ad
NL0bP <38990417+NL0bP@users.noreply.github.com>
2019-09-04 02:05:28 +0400

    [Game] Update CSCreateDoodadPacket.cs by ZeromusXYZ

--- AAEmu.Game/Core/Packets/C2G/CSCreateDoodadPacket.cs
+++ AAEmu.Game/Core/Packets/C2G/CSCreateDoodadPacket.cs
@@ -1,6 +1,7 @@
-using AAEmu.Commons.Network;
+﻿using AAEmu.Commons.Network;
 using AAEmu.Commons.Utils;
 using AAEmu.Game.Core.Network.Game;
+using AAEmu.Game.Models.Game.DoodadObj;
 
 namespace AAEmu.Game.Core.Packets.C2G
 {
@@ -21,6 +22,35 @@ namespace AAEmu.Game.Core.Packets.C2G
             var itemId = stream.ReadUInt64();
 
             _log.Warn("CreateDoodad, Id: {0}, X: {1}, Y: {2}, Z: {3}, ItemId: {4}", id, x, y, z, itemId);
+
+
+            var doodadSpawner = new DoodadSpawner();
+            doodadSpawner.Id = 0;
+            doodadSpawner.UnitId = id;
+            doodadSpawner.Position = Connection.ActiveChar.Position.Clone();
+            doodadSpawner.Position.X = x;
+            doodadSpawner.Position.Y = y;
+            doodadSpawner.Position.Z = z;
+            doodadSpawner.Position.RotationX = 0;
+            doodadSpawner.Position.RotationY = 0;
+            doodadSpawner.Position.RotationZ = 0;
+            doodadSpawner.Scale = scale;
+            var doodad = doodadSpawner.Spawn(0);
+            if (doodad == null)
+            {
+                _log.Warn("Doodad {0}, from spawn not exist at db", id);
+            }
+            else
+            {
+                doodad.OwnerType = DoodadOwnerType.Character;
+                doodad.OwnerId = Connection.ActiveChar.Id;
+                doodad.OwnerObjId = Connection.ActiveChar.ObjId;
+                //doodad.Position = new Models.Game.World.Point(Connection.ActiveChar.Position.WorldId, Connection.ActiveChar.Position.ZoneId, x, y, z, 0, 0, 0);
+                if (scale > 0)
+                    doodad.SetScale(scale);
+                doodad.Spawn();
+            }
+
         }
     }
 }
