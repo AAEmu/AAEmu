ecb28dbee9c841f5041cf2b00d85dba9b7007e96
SargeDG <58831721+SargeDG@users.noreply.github.com>
2019-12-23 16:36:01 +1300

    UpDated SCNoticeMessagePacket.cs

--- AAEmu.Game/Core/Packets/G2C/SCNoticeMessagePacket.cs
+++ AAEmu.Game/Core/Packets/G2C/SCNoticeMessagePacket.cs
@@ -1,4 +1,4 @@
-﻿using AAEmu.Commons.Network;
+using AAEmu.Commons.Network;
 using AAEmu.Game.Core.Network.Game;
 using AAEmu.Game.Models.Game.Chat;
 
@@ -6,17 +6,29 @@ namespace AAEmu.Game.Core.Packets.G2C
 {
     public class SCNoticeMessagePacket : GamePacket
     {
-        public SCNoticeMessagePacket() : base(SCOffsets.SCNoticeMessagePacket, 1)
+
+        //Initialize
+        readonly string _message = "";
+        readonly byte _type = 3;
+        readonly string _color = "9";
+        readonly int _vistime = 1000;
+
+        public SCNoticeMessagePacket(byte type, string color, int vistime, string message) : base(SCOffsets.SCNoticeMessagePacket, 1)
         {
+            _type = type;
+            _color = color;
+            _vistime = vistime;
+            _message = message;
+
 
         }
 
         public override PacketStream Write(PacketStream stream)
         {
-            stream.Write((byte)1); // noticeType
-            stream.Write("1"); // colorStr
-            stream.Write(20); // visibleTime
-            stream.Write("test"); // message
+            stream.Write(_type); // noticeType (1 - 3)
+            stream.Write(_color); // colorStr (Opacity 0 - 9)
+            stream.Write(_vistime); // visibleTime (Miliseconds)
+            stream.Write(_message); // message 
 
             return stream;
         }
