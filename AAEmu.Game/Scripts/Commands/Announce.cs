bcf79bb57806eae320afc12c7ac522a13581c4e9
ZeromusXYZ <zeromus.xyz@gmail.com>
2020-02-01 19:55:44 +0100

    [Game] -fix: /change_level and updated info comment on /announce

--- AAEmu.Game/Scripts/Commands/Announce.cs
+++ AAEmu.Game/Scripts/Commands/Announce.cs
@@ -1,48 +1,19 @@
 ﻿/*
 * Announce.cs
 * Author: SargeDG
-* usage: /Announce <Notice Type (3)> <Text opacity  (9)> <Visible time miliseconds (1000)>   <color Hex code + text message string (#00ff0dText here use as many spaces as you wish)>
-* usage example: /announce 3 9 5000 #00ff0dText here use as many spaces as you wish
+* Updated by: ZeromusXYZ
+* usage: /Announce <NoticeType> <Color> <VisibleTime> <message>
+* usage example: /announce 3 FFFF00 2500 Text here is in yellow
 *  
 * 
 * [Information]
-* _message is handled by the packet in such a way
-* were the first seven chars of _message are taken
-* as a Hex Color string(#00ff0d) 
-* 
-* those chars will not be displayed, for example
-* if we use the command (/announce 3 9 5000 server restart 20 mins)
-* then the text string displayed by SCNoticeMessagePacket
-* will display the string "restart 20 mins"
-* 
-* with hex color code appended to _message argument 
-* [example]
-* /announce 3 9 5000 #00ff0d server restart 20 mins
-* [/example]
-* the text string displayed by SCNoticeMessagePacket
-* will display the string "server restart 20 mins" 
-* 
-* 
-* Note: Hex Holor code seems to work fine with 
-* _color argument set to 9, this arg is wierd it seems to not only be able to control opacity 
-* but also affect color of text outside of the hex color code but in conjunction with the hex color code
-* 
-* This example will display the text in bright green acording to hex color code
-* [example]
-* /announce 3 9 5000 #00ff0d server restart 20 mins
-* [/example]
-* 
-* while this example will display the text in a dark blue 
-* [example]
-* /announce 3 99 5000 #00ff0d server restart 20 mins
-* [/example]
-* 
-* and this example will change the opacity of the text
-* [example]
-* /announce 3 4 5000 #00ff0d server restart 20 mins
-* [/example]
-* 
-* 
+* Check SCNoticeMessagePacket for information on how the packet
+* is constructed. It send as follows
+* 1 byte for the message type (default seems to be 3, but we don't know what the values mean)
+* 2 chars for a hex-string for alpha of the text color
+* 1 int for the time before it fades (in ms)
+* 6 chars for a hex-string RGB value of the text color
+* remainder is the message itself
 * 
 */
 
--- AAEmu.Game/Scripts/Commands/ChangeLevel.cs
+++ AAEmu.Game/Scripts/Commands/ChangeLevel.cs
@@ -2,6 +2,7 @@
 using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.Models.Game;
 using AAEmu.Game.Models.Game.Char;
+using AAEmu.Game.Models.Game.Skills;
 
 namespace AAEmu.Game.Scripts.Commands
 {
@@ -43,19 +44,40 @@ namespace AAEmu.Game.Scripts.Commands
                 character.SendMessage("[ChangeLevel] level: 1-100");
                 return;
             }
-            var expforlevel = ExpirienceManager.Instance.GetExpForLevel(level);
 
-            /*
+            var maxexptoadd = 0;
+
             if (character.Ability1 != AbilityType.None)
-                character.Abilities[character.Ability1].Exp += expforlevel;
-            */
-            
-            if (expforlevel <= 0)
-                return;
-            var diffExp = expforlevel - character.Expirience;
-            if (diffExp > 0)
-                character.AddExp(diffExp, true);
-            else
+            {
+                var expfora1 = ExpirienceManager.Instance.GetExpNeededToGivenLevel(character.Abilities.Abilities[character.Ability1].Exp, level);
+                if (expfora1 > maxexptoadd)
+                    maxexptoadd = expfora1;
+            }
+
+            if (character.Ability2 != AbilityType.None)
+            {
+                var expfora2 = ExpirienceManager.Instance.GetExpNeededToGivenLevel(character.Abilities.Abilities[character.Ability2].Exp, level);
+                if (expfora2 > maxexptoadd)
+                    maxexptoadd = expfora2;
+            }
+
+            if (character.Ability3 != AbilityType.None)
+            {
+                var expfora3 = ExpirienceManager.Instance.GetExpNeededToGivenLevel(character.Abilities.Abilities[character.Ability3].Exp, level);
+                if (expfora3 > maxexptoadd)
+                    maxexptoadd = expfora3;
+            }
+
+            var expforlevel = ExpirienceManager.Instance.GetExpForLevel(level) - character.Expirience;
+            if (expforlevel > maxexptoadd)
+                maxexptoadd = expforlevel;
+
+            // Add maximum required xp to get to target levels
+            if (maxexptoadd > 0)
+                character.AddExp(maxexptoadd, true);
+
+            // If the target level is bigger than player's current level, refill HP/MP and send level-up packet
+            if (level > character.Level)
             {
                 character.Level = level;
                 character.Expirience = expforlevel;
