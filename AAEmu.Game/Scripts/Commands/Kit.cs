8cee85e190dfcb5611f0918f12e8614d48048ff5
ZeromusXYZ <zeromus.xyz@gmail.com>
2020-02-20 20:03:56 +0100

    [Game] -add: /kit using kits.json
    Added kits.json that is now used by /kit to load item kit data
    Added /kit ? to get a list of available kits

--- AAEmu.Game/Scripts/Commands/Kit.cs
+++ AAEmu.Game/Scripts/Commands/Kit.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using AAEmu.Game.Core.Managers;
 using AAEmu.Game.Core.Managers.Id;
 using AAEmu.Game.Core.Packets.G2C;
@@ -6,30 +8,60 @@ using AAEmu.Game.Models.Game;
 using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.Items.Actions;
 using AAEmu.Game.Core.Managers.World;
+using Newtonsoft.Json;
+using NLog;
 
 namespace AAEmu.Game.Scripts.Commands
 {
     public class AddKit : ICommand
     {
+        private readonly Logger _log = LogManager.GetCurrentClassLogger();
 
         public class GMItemKitItem
         {
-            public string kitName;
-            public uint itemID;
-            public byte itemGrade;
-            public int itemCount;
+            [JsonProperty("names")]
+            public List<string> kitnames { get; set; }
+            [JsonProperty("id")]
+            public uint itemID { get; set; }
+            [JsonProperty("grade")]
+            public byte itemGrade { get; set; }
+            [JsonProperty("count")] 
+            public int itemCount { get; set; }
 
-            public GMItemKitItem(string name, uint id, byte grade = 0, int count = 1)
+            public GMItemKitItem()
             {
-                kitName = name.ToLower();
-                itemID = id;
-                itemGrade = grade;
-                itemCount = count;
+                kitnames = new List<string>();
+                itemID = 0;
+                itemGrade = 0;
+                itemCount = 1;
+            }
+
+            public GMItemKitItem(string kit, uint id, byte grade = 0, int count = 1)
+            {
+                this.kitnames.Clear();
+                this.kitnames.Add(kit.ToLower());
+                this.itemID = id;
+                this.itemGrade = grade;
+                this.itemCount = count;
+            }
+        }
+        
+        public class GMItemKitConfig
+        {
+            [JsonProperty("itemkits")]
+            public List<GMItemKitItem> itemkits = new List<GMItemKitItem>();
+
+            public List<string> itemkitnames = new List<string>();
+
+            public void Clear()
+            {
+                itemkits.Clear();
+                itemkitnames.Clear();
             }
         }
 
-        public List<GMItemKitItem> kits = new List<GMItemKitItem>();
-
+        public GMItemKitConfig kitconfig = new GMItemKitConfig();
+        
         public void OnLoad()
         {
             string[] name = { "kit", "addkit", "add_kit" };
@@ -53,6 +85,7 @@ namespace AAEmu.Game.Scripts.Commands
             if (args.Length == 0)
             {
                 character.SendMessage("[Items] " + CommandManager.CommandPrefix + "kit (target) <kitname>");
+                character.SendMessage("[Items] " + kitconfig.itemkitnames.Count.ToString() + " kits have been loaded, use " + CommandManager.CommandPrefix + "kit ?  to get a list of kits");
                 return;
             }
 
@@ -64,15 +97,30 @@ namespace AAEmu.Game.Scripts.Commands
             if (args.Length > firstarg + 0)
                 kitname = args[firstarg + 0].ToLower();
 
-            foreach(var kit in kits)
+            if (kitname == "?")
             {
-                if (kit.kitName != kitname)
+                character.SendMessage("[Items] " + CommandManager.CommandPrefix + "kit has the following kits registered:");
+                string s = string.Empty;
+                foreach (var n in kitconfig.itemkitnames)
+                    s += n + "  ";
+                character.SendMessage("|cFFFFFFFF" + s + "|r");
+                return;
+            }
+
+            //_log.Debug("foreach kit in kitconfig.itemkits");
+            foreach (var kit in kitconfig.itemkits)
+            {
+                //_log.Debug("kit.kitname: " + kit.kitname);
+                if (!kit.kitnames.Contains(kitname))
                     continue;
+
+                //_log.Debug("kit.itemID: " + kit.itemID.ToString());
 
                 var item = ItemManager.Instance.Create(kit.itemID, kit.itemCount, kit.itemGrade, true);
                 if (item == null)
                 {
                     character.SendMessage("|cFFFF0000Item could not be created, ID: {0} ! |r", kit.itemID);
+                    _log.Error("itemId not found: " + kit.itemID.ToString());
                     continue;
                 }
                 else
@@ -97,6 +145,7 @@ namespace AAEmu.Game.Scripts.Commands
                         targetPlayer.SendMessage("[GM] {0} has added a item to your inventory", character.Name);
                     }
                     itemsAdded++;
+                    //_log.Debug("kit.itemID: " + kit.itemID.ToString()+ " added to " + targetPlayer.Name);
                 }
 
             }
@@ -111,16 +160,38 @@ namespace AAEmu.Game.Scripts.Commands
             }
             else
             {
-                character.SendMessage("[Items] No items where for kit \"{0}\"", kitname);
+                character.SendMessage("[Items] No items in kit \"{0}\"", kitname);
             }
 
         }
 
         public void InitKits()
         {
-            kits.Clear();
-            kits.Add(new GMItemKitItem("test", 23865));
-            kits.Add(new GMItemKitItem("test", 23869));
+            //_log.Info("Init");
+            kitconfig.Clear();
+            
+            GMItemKitConfig jsonkit = new GMItemKitConfig();
+            try
+            {
+                string data = File.ReadAllText("Scripts\\Commands\\kits.json");
+                // _log.Info("data: " + data);
+                jsonkit = JsonConvert.DeserializeObject<GMItemKitConfig>(data);
+                kitconfig.itemkits.AddRange(jsonkit.itemkits);
+            }
+            catch (Exception x)
+            {
+                _log.Error("Exception: " + x.Message);
+            }
+            
+            // Create a enum for our "/kit ?" help command
+            foreach(var kit in kitconfig.itemkits)
+            {
+                foreach (var kn in kit.kitnames)
+                    if (!kitconfig.itemkitnames.Contains(kn))
+                        kitconfig.itemkitnames.Add(kn);
+            }
+            kitconfig.itemkitnames.Sort();
+
         }
     }
 }
