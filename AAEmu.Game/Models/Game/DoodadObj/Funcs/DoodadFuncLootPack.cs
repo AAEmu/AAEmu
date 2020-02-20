47287636d0ba07f925fd234c99e7764d58d097ac
zarobhr <30708321+zarobhr@users.noreply.github.com>
2020-02-20 16:53:51 -0500

    Doodad placement and removal fixes
    1. added /spawn remove xxxx to remove doodads
    2.improved DoodadFuncFakeUse to better the flow of doodads for phase funcs
    3. improved DoodadFuncUse for better flow od doodad funcs
    4.  doodad crops now completly removed from world when harvested
    5.doodad lootpacks now give items when crop harvested WIP all loot given
    6. can now remove doodads from world properly
    7. /spawn command improved for doodad and npc rotation
    8. added additional functions to math util for doodads

--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncLootPack.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncLootPack.cs
@@ -1,5 +1,11 @@
+using System.Collections.Generic;
+using AAEmu.Commons.Utils;
+using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.DoodadObj.Templates;
+using AAEmu.Game.Models.Game.Items;
 using AAEmu.Game.Models.Game.Units;
+using AAEmu.Game.Utils;
 
 namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
 {
@@ -10,6 +16,45 @@ namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
         public override void Use(Unit caster, Doodad owner, uint skillId)
         {
             _log.Debug("DoodadFuncLootPack : LootPackId {0}, SkillId {1}", LootPackId, skillId);
+            Character character = (Character)caster;
+            if (character == null) return;
+            var lootPacks = ItemManager.Instance.GetLootPacks(LootPackId); 
+            var dropRateMax = (uint)0;
+            var items = new List<Item>();
+            var groupNum = 0;
+            var groupFound  = false;
+            while (true)
+            {
+                groupNum += 1;
+                _log.Warn("DoodadFuncLootPack : LootPackId {0}, SkillId {1} Group Num {2}", LootPackId, skillId, groupNum);
+                groupFound = false;
+                for (var ui = 0; ui < lootPacks.Length; ui++)
+                {
+                    if (lootPacks[ui].Group == groupNum)
+                    {
+                        dropRateMax += lootPacks[ui].DropRate;
+                        groupFound = true;
+                    }
+                }
+                var dropRateItem = Rand.Next(0, dropRateMax);
+                var dropRateItemId = (uint)0;
+                for (var uii = 0; uii < lootPacks.Length; uii++)
+                {
+                    if ((lootPacks[uii].DropRate + dropRateItemId >= dropRateItem) && (lootPacks[uii].Group == groupNum))
+                    {
+                        int count = Rand.Next(lootPacks[uii].MinAmount, lootPacks[uii].MaxAmount);
+                        var item = ItemManager.Instance.Create(lootPacks[uii].ItemId, count, lootPacks[uii].GradeId);
+                        InventoryHelper.AddItemAndUpdateClient(character, item);
+                        break;
+                    }
+                    else
+                    {
+                        dropRateItemId += lootPacks[uii].DropRate;
+                    }
+                }
+                if (groupFound == false)
+                    break;
+            }
         }
     }
 }
