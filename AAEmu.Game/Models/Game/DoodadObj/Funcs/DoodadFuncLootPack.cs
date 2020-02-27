80a7606cba74e818223be79593316b2a9d114439
zarobhr <30708321+zarobhr@users.noreply.github.com>
2020-02-27 18:51:00 -0500

    Update DoodadFuncLootPack.cs
    removed all code lootpack, so my  pull request can be merged

--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncLootPack.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncLootPack.cs
@@ -16,50 +16,6 @@ namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
         public override void Use(Unit caster, Doodad owner, uint skillId)
         {
             _log.Debug("DoodadFuncLootPack : LootPackId {0}, SkillId {1}", LootPackId, skillId);
-            Character character = (Character)caster;
-            if (character == null) return;
-            var lootPacks = ItemManager.Instance.GetLootPacks(LootPackId); 
-            var dropRateMax = (uint)0;
-            var items = new List<Item>();
-            var groupNum = 0;
-            var groupFound  = false;
-            HashSet<int> hs = new HashSet<int>();
-            for (int i = 0; i < lootPacks.Length; i++)
-            {
-                hs.Add(lootPacks[i].Group);
-            }
-            while (groupNum < hs.Count)
-            {
-                groupNum += 1;
-                _log.Warn("DoodadFuncLootPack : LootPackId {0}, SkillId {1} Group Num {2}", LootPackId, skillId, groupNum);
-                groupFound = false;
-                for (var ui = 0; ui < lootPacks.Length; ui++)
-                {
-                    if (lootPacks[ui].Group == groupNum)
-                    {
-                        dropRateMax += lootPacks[ui].DropRate;
-                        groupFound = true;
-                    }
-                }
-                var dropRateItem = Rand.Next(0, dropRateMax);
-                var dropRateItemId = (uint)0;
-                for (var uii = 0; uii < lootPacks.Length; uii++)
-                {
-                    if ((lootPacks[uii].DropRate + dropRateItemId >= dropRateItem) && (lootPacks[uii].Group == groupNum))
-                    {
-                        int count = Rand.Next(lootPacks[uii].MinAmount, lootPacks[uii].MaxAmount);
-                        var item = ItemManager.Instance.Create(lootPacks[uii].ItemId, count, lootPacks[uii].GradeId);
-                        InventoryHelper.AddItemAndUpdateClient(character, item);
-                        break;
-                    }
-                    else
-                    {
-                        dropRateItemId += lootPacks[uii].DropRate;
-                    }
-                }
-                if (groupFound == false)
-                    break;
-            }
-        }
+        }  
     }
 }
