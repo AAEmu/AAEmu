67fa9ba8b9af01ca5152bdd47b7961229079fc9f
NL0bP <38990417+NL0bP@users.noreply.github.com>
2020-04-19 21:04:00 +0400

    [Game] fixed:  lamp on/off

--- AAEmu.Game/Models/Game/DoodadObj/DoodadFunc.cs
+++ AAEmu.Game/Models/Game/DoodadObj/DoodadFunc.cs
@@ -2,11 +2,13 @@
 using AAEmu.Game.Core.Managers.UnitManagers;
 using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.Models.Game.Units;
+using NLog;
 
 namespace AAEmu.Game.Models.Game.DoodadObj
 {
     public class DoodadFunc
     {
+        protected static Logger _log = LogManager.GetCurrentClassLogger();
         public uint GroupId { get; set; }
         public uint FuncId { get; set; }
         public string FuncType { get; set; }
@@ -18,7 +20,10 @@ namespace AAEmu.Game.Models.Game.DoodadObj
 
         public async void Use(Unit caster, Doodad owner, uint skillId)
         {
-            owner.GrowthTime = DateTime.Now;
+            _log.Debug("DoodadFunc : skillId {0}, GroupId {1}, FuncId {2}, FuncType {3}, NextPhase {4}, SoundId {5}, SkillId {6}, PermId {7}, Count {8}",
+                skillId, GroupId, FuncId, FuncType, NextPhase, SoundId, SkillId, PermId, Count);
+
+            owner.GrowthTime = DateTime.MinValue;
             var template = DoodadManager.Instance.GetFuncTemplate(FuncId, FuncType);
 
             if (template == null)
@@ -34,13 +39,17 @@ namespace AAEmu.Game.Models.Game.DoodadObj
                     owner.FuncTask = null;
                 }
 
+                if (SoundId > 0)
+                {
+                    owner.BroadcastPacket(new SCDoodadSoundPacket(owner, SoundId), true); // TODO added since some doodad have sound
+                }
+
                 owner.FuncGroupId = (uint)NextPhase;
-
-                owner.BroadcastPacket(new SCDoodadPhaseChangedPacket(owner), false); // FIX: added to work on/off lighting and destruction of drums/boxes
-
                 var funcs = DoodadManager.Instance.GetPhaseFunc(owner.FuncGroupId);
                 foreach (var func in funcs)
-                    func.Use(caster, owner, skillId);
+                {
+                    func?.Use(caster, owner, skillId);
+                }
             }
         }
     }
