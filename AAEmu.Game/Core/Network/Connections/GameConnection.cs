808c2719b2c329d1d6321f344f82c6dc170086a0
ZeromusXYZ <zeromus.xyz@gmail.com>
2020-05-03 22:16:32 +0200

    [Game] fix: Potential or partial fix for disconnects from raid
    Changed code so Character no longer release their ObjId, but insteads links it to the Character.Id in a dictionary. That way as long as the server is running, the server will always assign the same ObjId to the same character, and this way can't break the raid/party window's selections.
    Still needs more finetuning related to offline status display when moving members around.

--- AAEmu.Game/Core/Network/Connections/GameConnection.cs
+++ AAEmu.Game/Core/Network/Connections/GameConnection.cs
@@ -228,14 +228,17 @@ namespace AAEmu.Game.Core.Network.Connections
                 SendPacket(new SCCancelCharacterDeleteResponsePacket(characterId, 4));
             }
         }
-
+        /// <summary>
+        /// Called when closing a connection
+        /// </summary>
         public void Save()
         {
             if (ActiveChar == null)
                 return;
 
             ActiveChar.Delete();
-            ObjectIdManager.Instance.ReleaseId(ActiveChar.ObjId);
+            // Removed ReleaseId here to try and fix party/raid disconnect and reconnect issues. Replaced with saving the data
+            //ObjectIdManager.Instance.ReleaseId(ActiveChar.ObjId);
 
             ActiveChar.Save();
         }
--- AAEmu.Game/Models/Game/Char/Character.cs
+++ AAEmu.Game/Models/Game/Char/Character.cs
@@ -47,6 +47,7 @@ namespace AAEmu.Game.Models.Game.Char
     public sealed class Character : Unit
     {
         private static Logger _log = LogManager.GetCurrentClassLogger();
+        public static Dictionary<uint, uint> _usedCharacterObjIds = new Dictionary<uint, uint>();
 
         private Dictionary<ushort, string> _options;
 
