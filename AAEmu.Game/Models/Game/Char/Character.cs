07a9a78eede614690191c2f839b15d6bea0c3bf6
ZeromusXYZ <zeromus.xyz@gmail.com>
2020-05-10 19:34:22 +0200

    [Game] fix: Fixed some merge weirdness

--- AAEmu.Game/Models/Game/Char/Character.cs
+++ AAEmu.Game/Models/Game/Char/Character.cs
@@ -721,18 +721,16 @@ namespace AAEmu.Game.Models.Game.Char
 
         public void AddExp(int exp, bool shouldAddAbilityExp)
         {
-            var expMultiplier = ConfigurationManager.Instance.GetConfiguration("ExperienceMultiplier");
+            var expMultiplier = 1d;
             if (exp == 0)
                 return;
-            var expMultiplier = 1f;
             if (float.TryParse(ConfigurationManager.Instance.GetConfiguration("ExperienceMultiplierInPercent"), out var xpm))
                 expMultiplier = xpm / 100f;
-            var expToGive = Math.Round(expMultiplier * exp);
-            exp = (int)expToGive;
+            var totalExp = Math.Round(expMultiplier * exp);
+            exp = (int)totalExp;
             Expirience += exp;
             if (shouldAddAbilityExp)
                 Abilities.AddActiveExp(exp); // TODO ... or all?
-            if(int.Parse(expMultiplier) > 0) exp = exp * int.Parse(expMultiplier);
             SendPacket(new SCExpChangedPacket(ObjId, exp, shouldAddAbilityExp));
             CheckLevelUp();
         }
