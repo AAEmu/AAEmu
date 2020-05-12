329f93235413cc6acf6bfae8a67713ea5d182b29
NL0bP <38990417+NL0bP@users.noreply.github.com>
2020-05-12 04:04:08 +0400

    [Game] fixed: Npc now deal damage when attacking a character

--- AAEmu.Game/Models/Game/NPChar/Npc.cs
+++ AAEmu.Game/Models/Game/NPChar/Npc.cs
@@ -368,6 +368,163 @@ namespace AAEmu.Game.Models.Game.NPChar
             }
         }
 
+                public override int Dps
+        {
+            get
+            {
+                var weapon = (Weapon)Equip[(int)EquipmentItemSlot.Mainhand];
+                var res = weapon?.Dps ?? 0;
+                res += Str / 10f;
+                foreach (var bonus in GetBonuses(UnitAttribute.MainhandDps))
+                {
+                    if (bonus.Template.ModifierType == UnitModifierType.Percent)
+                        res += (int)(res * bonus.Value / 100f);
+                    else
+                        res += bonus.Value;
+                }
+
+                return (int)(res * 1000);
+            }
+        }
+
+        public override int DpsInc
+        {
+            get
+            {
+                var formula =
+                    FormulaManager.Instance.GetUnitFormula(FormulaOwnerType.Npc, UnitFormulaKind.MeleeDpsInc);
+                var parameters = new Dictionary<string, double>();
+                parameters["level"] = Level;
+                parameters["str"] = Str;
+                parameters["dex"] = Dex;
+                parameters["sta"] = Sta;
+                parameters["int"] = Int;
+                parameters["spi"] = Spi;
+                parameters["fai"] = Fai;
+                var res = formula.Evaluate(parameters);
+                foreach (var bonus in GetBonuses(UnitAttribute.MeleeDpsInc))
+                {
+                    if (bonus.Template.ModifierType == UnitModifierType.Percent)
+                        res += (res * bonus.Value / 100f);
+                    else
+                        res += bonus.Value;
+                }
+
+                return (int)res;
+            }
+        }
+
+        public override int OffhandDps
+        {
+            get
+            {
+                var weapon = (Weapon)Equip[(int)EquipmentItemSlot.Offhand];
+                var res = weapon?.Dps ?? 0;
+                res += Str / 10f;
+                foreach (var bonus in GetBonuses(UnitAttribute.OffhandDps))
+                {
+                    if (bonus.Template.ModifierType == UnitModifierType.Percent)
+                        res += (int)(res * bonus.Value / 100f);
+                    else
+                        res += bonus.Value;
+                }
+
+                return (int)(res * 1000);
+            }
+        }
+
+        public override int RangedDps
+        {
+            get
+            {
+                var weapon = (Weapon)Equip[(int)EquipmentItemSlot.Ranged];
+                var res = weapon?.Dps ?? 0;
+                res += Dex / 10f;
+                foreach (var bonus in GetBonuses(UnitAttribute.RangedDps))
+                {
+                    if (bonus.Template.ModifierType == UnitModifierType.Percent)
+                        res += (int)(res * bonus.Value / 100f);
+                    else
+                        res += bonus.Value;
+                }
+
+                return (int)(res * 1000);
+            }
+        }
+
+        public override int RangedDpsInc
+        {
+            get
+            {
+                var formula =
+                    FormulaManager.Instance.GetUnitFormula(FormulaOwnerType.Npc, UnitFormulaKind.RangedDpsInc);
+                var parameters = new Dictionary<string, double>();
+                parameters["level"] = Level;
+                parameters["str"] = Str;
+                parameters["dex"] = Dex;
+                parameters["sta"] = Sta;
+                parameters["int"] = Int;
+                parameters["spi"] = Spi;
+                parameters["fai"] = Fai;
+                var res = formula.Evaluate(parameters);
+                foreach (var bonus in GetBonuses(UnitAttribute.RangedDpsInc))
+                {
+                    if (bonus.Template.ModifierType == UnitModifierType.Percent)
+                        res += (res * bonus.Value / 100f);
+                    else
+                        res += bonus.Value;
+                }
+
+                return (int)res;
+            }
+        }
+
+        public override int MDps
+        {
+            get
+            {
+                var weapon = (Weapon)Equip[(int)EquipmentItemSlot.Mainhand];
+                var res = weapon?.MDps ?? 0;
+                res += Int / 10f;
+                foreach (var bonus in GetBonuses(UnitAttribute.SpellDps))
+                {
+                    if (bonus.Template.ModifierType == UnitModifierType.Percent)
+                        res += (int)(res * bonus.Value / 100f);
+                    else
+                        res += bonus.Value;
+                }
+
+                return (int)(res * 1000);
+            }
+        }
+
+        public override int MDpsInc
+        {
+            get
+            {
+                var formula =
+                    FormulaManager.Instance.GetUnitFormula(FormulaOwnerType.Npc, UnitFormulaKind.SpellDpsInc);
+                var parameters = new Dictionary<string, double>();
+                parameters["level"] = Level;
+                parameters["str"] = Str;
+                parameters["dex"] = Dex;
+                parameters["sta"] = Sta;
+                parameters["int"] = Int;
+                parameters["spi"] = Spi;
+                parameters["fai"] = Fai;
+                var res = formula.Evaluate(parameters);
+                foreach (var bonus in GetBonuses(UnitAttribute.SpellDpsInc))
+                {
+                    if (bonus.Template.ModifierType == UnitModifierType.Percent)
+                        res += (res * bonus.Value / 100f);
+                    else
+                        res += bonus.Value;
+                }
+
+                return (int)res;
+            }
+        }
+
         public override int Armor
         {
             get
