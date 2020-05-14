ba8d15eba39f9b0378594ec4e0e6ccc73c4cc302
NLObP <38990417+NL0bP@users.noreply.github.com>
2020-05-15 00:04:40 +0400

    [SQL] fixed: Items table

--- SQL/updates/2020-05-14_aaemu_game_items.sql
+++ SQL/updates/2020-05-14_aaemu_game_items.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `aaemu_game.items` DROP COLUMN `bounded`;
-ALTER TABLE `aaemu_game.items` ADD `flags` UNSIGNED tinyint(3) NOT NULL AFTER `grade`;
+ALTER TABLE `aaemu_game.items` ADD `flags` tinyint(3) unsigned NOT NULL AFTER `grade`;
