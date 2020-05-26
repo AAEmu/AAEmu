ee92455400d5eae92a298417f9700909a8e4c8b1
Casey Tucker <Caseybr0Tucker@live.com>
2020-05-25 23:47:34 -0400

    Added streetlamp support with new doodadfunc structure

--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncFakeUse.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncFakeUse.cs
@@ -14,22 +14,7 @@ namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
         public override void Use(Unit caster, Doodad owner, uint skillId)
         {
             _log.Warn("DoodadFuncFakeUse : SkillId {0}, skillId {1}, FakeSkillId {2}, TargetParent {3}", SkillId, skillId, FakeSkillId, TargetParent);
-
-            //var sound = owner.Template.FuncGroups[(int)owner.FuncGroupId].SoundId;
-            var func = DoodadManager.Instance.GetFunc(owner.FuncGroupId, skillId);
-            if (func?.SoundId > 0)
-            {
-                owner.BroadcastPacket(new SCDoodadSoundPacket(owner, func.SoundId), true); // добавил? так как у некоторых Doodad есть звук
-            }
-            // added by robert
-            if (func.NextPhase > 0)
-            {
-                owner.FuncGroupId = (uint)func.NextPhase;
-                owner.BroadcastPacket(new SCDoodadPhaseChangedPacket(owner), false); // FIX: added to work on/off lighting and destruction of drums/boxes
-                var nextfunc = DoodadManager.Instance.GetFunc(owner.FuncGroupId, 0);
-                if (nextfunc != null)
-                    nextfunc.Use(caster, owner, skillId);
-            }
+            DoodadManager.Instance.TriggerPhaseFunc(GetType().Name, owner.FuncGroupId, caster, owner, skillId);
 
         }
     }
--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncTod.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncTod.cs
@@ -1,21 +1,24 @@
-﻿using AAEmu.Game.Core.Packets.G2C;
+﻿using System;
+using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Core.Managers.UnitManagers;
+using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.Models.Game.DoodadObj.Templates;
 using AAEmu.Game.Models.Game.Units;
+using AAEmu.Game.Models.Tasks.Doodads;
 
 namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
 {
     public class DoodadFuncTod : DoodadFuncTemplate
-    {
+    {//This func is typically the end of the chain
         public int Tod { get; set; }
         public uint NextPhase { get; set; }
         
         public override void Use(Unit caster, Doodad owner, uint skillId)
         {
-            _log.Debug("DoodadFuncTod : NextPhase {0}, SkillId {1}", NextPhase, skillId);
-
-            if (owner.FuncTask == null)
+            //_log.Debug("DoodadFuncTod : NextPhase {0}, SkillId {1}", NextPhase, skillId);
+            if (NextPhase > 0)
             {
-                // выполняем действие
+                owner.FuncGroupId = NextPhase;
                 owner.BroadcastPacket(new SCDoodadPhaseChangedPacket(owner), true);
             }
         }
