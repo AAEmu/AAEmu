6dd34eac782757ec4f330edd73154571ac238d5a
Unknown <gene.aagenesis@gmail.com>
2020-05-30 08:27:43 +0200

    WIP

--- AAEmu.Game/Models/Game/Skills/Plots/New/NewPlotCondition.cs
+++ AAEmu.Game/Models/Game/Skills/Plots/New/NewPlotCondition.cs
@@ -4,7 +4,7 @@ namespace AAEmu.Game.Models.Game.Skills.Plots.New
     {
         public int Id { get; set; }
         public bool NotCondition { get; set; }
-        public int Kind { get; set; }
+        public PlotConditionType Kind { get; set; }
         public int Param1 { get; set; }
         public int Param2 { get; set; }
         public int Param3 { get; set; }
