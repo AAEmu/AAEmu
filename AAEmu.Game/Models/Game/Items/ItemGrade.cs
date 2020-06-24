9b95ef661b0abd93f9f155fb21fa189a31e92bc9
Mario Alexandre Lopes Liberato <mliberato@gs2012.xyz>
2020-06-24 05:51:55 +0100

    Moved enum to its own file.

---
+++ AAEmu.Game/Models/Game/Items/ItemGrade.cs
@@ -0,0 +1,18 @@
+﻿namespace AAEmu.Game.Models.Game.Items
+{
+    public enum ItemGrade : byte
+    {
+        Crude = 0,
+        Basic = 1,
+        Grand = 2,
+        Rare = 3,
+        Arcane = 4,
+        Heroic = 5,
+        Unique = 6,
+        Celestial = 7,
+        Divine = 8,
+        Epic = 9,
+        Legendary = 10,
+        Mythic = 11
+    }
+}
