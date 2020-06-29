0b1ff31abd831dd7282ce0e771654482913c1ab4
ZeromusXYZ <zeromus.xyz@gmail.com>
2020-06-29 18:10:13 +0200

    [GM] fix: wrong using statement in AddItem
    Somehow the wrong namespace made it into here.

--- AAEmu.Game/Scripts/Commands/AddItem.cs
+++ AAEmu.Game/Scripts/Commands/AddItem.cs
@@ -3,7 +3,7 @@ using AAEmu.Game.Core.Managers;
 using AAEmu.Game.Core.Managers.Id;
 using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.Models.Game;
-using AAEmu.Game.Models.ItemGrade;
+using AAEmu.Game.Models.Game.Items;
 using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.Items.Actions;
 using AAEmu.Game.Core.Managers.World;
