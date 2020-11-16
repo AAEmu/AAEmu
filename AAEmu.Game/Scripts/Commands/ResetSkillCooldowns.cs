34b7d402455e0227fe9ea3b7e023e8669021aad8
Hexlulz <hexaagenesis@gmail.com>
2020-11-16 17:06:04 -0600

    [Commands] Reset All Skill Cooldowns

---
+++ AAEmu.Game/Scripts/Commands/ResetSkillCooldowns.cs
@@ -0,0 +1,32 @@
+﻿using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Core.Packets.G2C;
+using AAEmu.Game.Models.Game;
+using AAEmu.Game.Models.Game.Char;
+using AAEmu.Game.Core.Managers.World;
+
+namespace AAEmu.Game.Scripts.Commands
+{
+    public class ResetSkillCooldowns : ICommand
+    {
+        public void OnLoad()
+        {
+            string[] name = { "resetcd", "resetskillcooldowns", "rcd"};
+            CommandManager.Instance.Register(name, this);
+        }
+
+        public string GetCommandLineHelp()
+        {
+            return "";
+        }
+
+        public string GetCommandHelpText()
+        {
+            return "Resets skill cooldowns.";
+        }
+
+        public void Execute(Character character, string[] args)
+        {
+            character.ResetAllSkillCooldowns(false);
+        }
+    }
+}
