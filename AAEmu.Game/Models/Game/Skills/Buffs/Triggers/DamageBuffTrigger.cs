0e846bf48f9eb9a62caf9f9fe54d68ff5765e619
Hexlulz <hexaagenesis@gmail.com>
2020-11-19 13:57:07 -0600

    [Buffs] Trying to Add Effect

--- AAEmu.Game/Models/Game/Skills/Buffs/Triggers/DamageBuffTrigger.cs
+++ AAEmu.Game/Models/Game/Skills/Buffs/Triggers/DamageBuffTrigger.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Models.Game.Skills.Effects;
 using AAEmu.Game.Models.Game.Units;
 
 namespace AAEmu.Game.Models.Game.Skills.Buffs.Triggers
@@ -15,7 +16,13 @@ namespace AAEmu.Game.Models.Game.Skills.Buffs.Triggers
             var caster = _owner.Caster;
             var target = _owner.Caster;
             _log.Warn("Buff[{0}] damage trigger executed. Applying Effect[{1}]!", _owner.Template.BuffId, Template.Effect.Id);
-            //Template.Effect.Apply()
+
+            /*
+            var skillCaster = new SkillCasterUnit(caster.ObjId);
+            var skillCastTarget = new SkillCastUnitTarget(caster.ObjId);
+            var effect = new Effect(caster, caster, skillCaster, Template.Effect, null, DateTime.Now);
+            var skillObj = new SkillObject();
+            Template.Effect.Apply(caster, skillCaster, caster, skillCastTarget, new CastBuff(effect), new EffectSource(_owner.Template), skillObj, DateTime.Now*/
         }
 
         public DamageBuffTrigger(Effect owner, BuffTriggerTemplate template) : base(owner, template)
