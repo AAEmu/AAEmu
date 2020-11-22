51624df8f6d68dd50f91fd23895fc1d465b4cd68
AAGene <gene.aagenesis@gmail.com>
2020-11-22 16:18:36 +0100

    [Doodads] Remove rats spawning at 'random'

--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncRatioRespawn.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncRatioRespawn.cs
@@ -17,42 +17,6 @@ namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
         public override void Use(Unit caster, Doodad owner, uint skillId, int nextPhase = 0)
         {
             _log.Debug("DoodadFuncRatioRespawn : Ratio {0}, SpawnDoodadId {1}", Ratio, SpawnDoodadId);
-
-            if (caster is Character character)
-            {
-                var (newX, newY) = MathUtil.AddDistanceToFront(1, character.Position.X, character.Position.Y, character.Position.RotationZ); //TODO расстояние вперед 1 м
-
-                // NPC spawn
-                var npcSpawner = new NpcSpawner
-                {
-                    Id = 0,
-                    RespawnTime = 0, // not appear after death
-                    UnitId = 7503,   // Rat Id = 7503
-                    Position = character.Position.Clone()
-                };
-                npcSpawner.Position.X = newX + Rand.Next(-2, 2);
-                npcSpawner.Position.Y = newY + Rand.Next(-2, 2);
-                npcSpawner.Position.Z = AppConfiguration.Instance.HeightMapsEnable
-                    ? WorldManager.Instance.GetHeight(npcSpawner.Position.ZoneId, npcSpawner.Position.X, npcSpawner.Position.Y)
-                    : npcSpawner.Position.Z;
-                // looks in the direction of the character
-                var angle = MathUtil.CalculateAngleFrom(npcSpawner.Position.X, npcSpawner.Position.Y, character.Position.X, character.Position.Y);
-                var rotZ = MathUtil.ConvertDegreeToDirection(angle);
-                npcSpawner.Position.RotationX = 0;
-                npcSpawner.Position.RotationY = 0;
-                npcSpawner.Position.RotationZ = rotZ;
-                var npc = npcSpawner.Spawn(0);
-                npc.Respawn = DateTime.MinValue; // not appear after death
-
-
-                var target = (BaseUnit)character;
-                npc.CurrentTarget = target;
-                npc.SetForceAttack(true);
-                npc.IsAutoAttack = true;
-
-                var combat = new Combat();
-                combat.Execute(npc);
-            }
         }
     }
 }
