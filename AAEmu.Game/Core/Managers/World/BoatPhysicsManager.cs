787f732c59cc6bc51495806df894963f42b501b4
AAGene <gene.aagenesis@gmail.com>
2020-12-07 17:32:34 +0100

    [Boats] Basic physics, need collision testing

--- AAEmu.Game/Core/Managers/World/BoatPhysicsManager.cs
+++ AAEmu.Game/Core/Managers/World/BoatPhysicsManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Numerics;
 using System.Threading;
 using AAEmu.Commons.Utils;
@@ -6,16 +7,35 @@ using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.Models.Game.Slaves;
 using AAEmu.Game.Models.Game.Units;
 using AAEmu.Game.Models.Game.Units.Movements;
+using AAEmu.Game.Physics.Forces;
 using AAEmu.Game.Utils;
+using Jitter.Collision;
+using Jitter.Collision.Shapes;
+using Jitter.Dynamics;
+using Jitter.LinearMath;
+using NLog;
 
 namespace AAEmu.Game.Core.Managers.World
 {
     public class BoatPhysicsManager : Singleton<BoatPhysicsManager>
     {
         private Thread thread;
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+        
+        private CollisionSystem _collisionSystem;
+        private Jitter.World _physWorld;
+        private Dictionary<uint, RigidBody> _rigidBodies;
+        private Buoyancy _buoyancy;
 
         public void Initialize()
         {
+            _collisionSystem = new CollisionSystemSAP();
+            _physWorld = new Jitter.World(_collisionSystem);
+            _buoyancy = new Buoyancy(_physWorld);
+            // _buoyancy.UseOwnFluidArea(DefineFluidArea());
+            _buoyancy.FluidBox = new JBBox(new JVector(0, 0, 0), new JVector(100000, 99, 100000));
+            _rigidBodies = new Dictionary<uint, RigidBody>();
+            
             thread = new Thread(PhysicsThread);
             thread.Start();
         }
@@ -25,15 +45,34 @@ namespace AAEmu.Game.Core.Managers.World
             while (Thread.CurrentThread.IsAlive)
             {
                 Thread.Sleep(50);
+                _physWorld.Step(1 / 20.0f, false);
 
                 foreach (var slave in SlaveManager.Instance.GetActiveSlavesByKinds(new[] {SlaveKind.BigSailingShip, SlaveKind.Boat, SlaveKind.Fishboat, SlaveKind.SmallSailingShip, SlaveKind.MerchantShip, SlaveKind.Speedboat}))
                 {
-                    BoatPhysicsTick(slave);
+                    if (slave.SpawnTime.AddSeconds(8) > DateTime.Now)
+                        continue;
+                    
+                    if (!_rigidBodies.ContainsKey(slave.TlId))
+                    {
+                        var rigidBody = new RigidBody(new BoxShape(4.0f, 8.0f, 7.0f));
+                        rigidBody.Position = new JVector(slave.Position.X, slave.Position.Z, slave.Position.Y);
+                        
+                        _rigidBodies.Add(slave.TlId, rigidBody);
+                        _buoyancy.Add(rigidBody, 3);
+                        _physWorld.AddBody(rigidBody);
+                    }
+
+                    var slaveRigidBody = _rigidBodies[slave.TlId];
+                    
+                    slave.Position.X = slaveRigidBody.Position.X;
+                    slave.Position.Y = slaveRigidBody.Position.Z;
+                    slave.Position.Z = slaveRigidBody.Position.Y;
+                    BoatPhysicsTick(slave, slaveRigidBody); 
                 }
             }
         }
 
-        public void BoatPhysicsTick(Slave slave)
+        public void BoatPhysicsTick(Slave slave, RigidBody rigidBody)
         {
             var moveType = (ShipMoveType)MoveType.GetType(MoveTypeEnum.Ship);
             moveType.UseSlaveBase(slave);
@@ -66,17 +105,21 @@ namespace AAEmu.Game.Core.Managers.World
                 slave.Speed -= (slave.Speed / 45);
             
             slave.Position.RotationZ = MathUtil.ConvertDegreeToDirection(slave.RotationDegrees);
-            
-            var (newX, newY) = MathUtil.AddDistanceToFrontDeg(-(slave.Speed / 20f), slave.Position.X, slave.Position.Y,
-                slave.RotationDegrees - 90.0f );
 
-            var diffX = newX - slave.Position.X;
-            var diffY = newY - slave.Position.Y;
+            var slaveRotRad = (slave.RotationDegrees + 90) * (Math.PI / 180.0f);
             
-            slave.Move(diffX, diffY, 0);
+            // var (newX, newY) = MathUtil.AddDistanceToFrontDeg(-(slave.Speed / 20f), slave.Position.X, slave.Position.Y,
+            //     slave.RotationDegrees - 90.0f );
+            //
+            // var diffX = newX - slave.Position.X;
+            // var diffY = newY - slave.Position.Y;
             
-            moveType.VelX = (short) (diffX * 21900);
-            moveType.VelY = (short) (diffY * 21900);
+            // slave.Move(diffX, diffY, 0);
+            //
+            // moveType.VelX = (short) (diffX * 21900);
+            // moveType.VelY = (short) (diffY * 21900);
+            
+            rigidBody.AddForce(new JVector(slave.Throttle * 50 * (float)Math.Cos(slaveRotRad), 0.0f, slave.Throttle * 50 * (float)Math.Sin(slaveRotRad)));
             
             slave.RotationDegrees -= slave.RotSpeed;
             // TODO : Find a better way
--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncRequireQuest.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncRequireQuest.cs
@@ -10,7 +10,7 @@ namespace AAEmu.Game.Models.Game.DoodadObj.Funcs
         
         public override void Use(Unit caster, Doodad owner, uint skillId, int nextPhase = 0)
         {
-            _log.Debug("DoodadFuncRequireQuest");
+            _log.Debug("DoodadFuncRequireQuest QuestId: {0}, WorldIntId {1}", QuestId, WorldInteractionId);
         }
     }
 }
