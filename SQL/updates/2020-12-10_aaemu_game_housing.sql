2378375809597f4d5dded1255a1c2ea98fb74072
ZeromusXYZ <zeromus.xyz@gmail.com>
2020-12-10 19:32:00 +0100

    [SQL] Fixed two typos

--- SQL/updates/2020-12-10_aaemu_game_housing.sql
+++ SQL/updates/2020-12-10_aaemu_game_housing.sql
@@ -3,8 +3,8 @@
 -- -------------------------------------------------
 ALTER TABLE `housings` 
 ADD COLUMN `place_date` DATETIME NOT NULL DEFAULT NOW() AFTER `permission`,
-ADD COLUMN `protected_until` DATETIME NOT NULL DEFAULT NOW() AFTER `place_date`;
-ADD COLUMN `faction_id` INT UNSIGNED NOT NULL DEFAULT 1 AFTER `protected_until`;
+ADD COLUMN `protected_until` DATETIME NOT NULL DEFAULT NOW() AFTER `place_date`,
+ADD COLUMN `faction_id` INT UNSIGNED NOT NULL DEFAULT 1 AFTER `protected_until`,
 ADD COLUMN `sell_to` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `faction_id`,
 ADD COLUMN `sell_price` BIGINT(20) NOT NULL DEFAULT 0 AFTER `sell_to`;
 
