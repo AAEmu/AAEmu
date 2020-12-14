43cbfb7448daeb49eb35897cc2d61a8b5b47644f
AAGene <gene.aagenesis@gmail.com>
2020-12-14 01:58:06 +0100

    [AI] Add all default AI types

---
+++ AAEmu.Game/Models/Game/AI/UnitTypes/BigMonsterHoldPositionAI.cs
@@ -0,0 +1,12 @@
+using AAEmu.Game.Models.Game.AI.Framework;
+
+namespace AAEmu.Game.Models.Game.AI.UnitTypes
+{
+    public class BigMonsterHoldPositionAI : AbstractUnitAI
+    {
+        public override Framework.States GetNextState(State previous)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
