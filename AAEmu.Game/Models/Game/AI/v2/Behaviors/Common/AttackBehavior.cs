a012a2b27acee13e2404758f7ae75c1aee1adb35
AAGene <gene.aagenesis@gmail.com>
2020-12-27 00:35:33 +0100

    [AI] Hackfix: Melee attack range

--- AAEmu.Game/Models/Game/AI/v2/Behaviors/Common/AttackBehavior.cs
+++ AAEmu.Game/Models/Game/AI/v2/Behaviors/Common/AttackBehavior.cs
@@ -48,6 +48,9 @@ namespace AAEmu.Game.Models.Game.AI.v2.Behaviors
             var pickedSkillId = (uint)Ai.Owner.Template.BaseSkillId;
             if (skills.Count > 0)
                 pickedSkillId = skills[Rand.Next(skills.Count)].SkillId;
+            // Hackfix for Melee attack. Needs to look at the held weapon (if any) or default to 3m. 
+            if (pickedSkillId == 2 && trgDist > 4.0f)
+                return;
             
             var skillTemplate = SkillManager.Instance.GetSkillTemplate(pickedSkillId);
             var skill = new Skill(skillTemplate);
