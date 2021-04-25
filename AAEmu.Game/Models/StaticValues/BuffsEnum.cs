53fa29e3550c2152bb8545df72e1eec16bf06526
ZeromusXYZ <zeromus.xyz@gmail.com>
2021-04-25 08:07:27 +0200

    [Game] Magic numbers (#266)
    * [Game] Added some files for magic numbers

---
+++ AAEmu.Game/Models/StaticValues/BuffsEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace AAEmu.Game.Models.StaticValues
+{
+    public static class BuffsEnum
+    {
+        public const uint Untouchable = 545;
+        public const uint RemovalDebuff = 2250; // for houses
+    }
+ 
+}
