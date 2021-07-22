012ab673a2dbc52d319bbc1a283ce5843cb1dcc9
ZeromusXYZ <zeromus.xyz@gmail.com>
2021-07-22 19:00:55 +0200

    [Game] New character spawn update
    - Updated the rotations for newly created characters.
    - Added code so this can be defined in degrees in CharTemplates.json
    - Updated /teleport Lilyut to actually send you to Lilyut instead of Solzreed
    - Added the missing DROP fields to the previously create 2021-07-21_aaemu_game_transform.sql

--- SQL/updates/2021-07-21_aaemu_game_transform.sql
+++ SQL/updates/2021-07-21_aaemu_game_transform.sql
@@ -5,3 +5,13 @@ ALTER TABLE `characters`
 DROP `rotation_x`,
 DROP `rotation_y`,
 DROP `rotation_z`;
+
+ALTER TABLE `housings`
+DROP `rotation_x`,
+DROP `rotation_y`,
+DROP `rotation_z`;
+
+ALTER TABLE `doodads`
+DROP `rotation_x`,
+DROP `rotation_y`,
+DROP `rotation_z`;
