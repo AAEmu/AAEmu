d5c80ef8a928e78e7d991fc864208e3f9bea80c0
ZeromusXYZ <zeromus.xyz@gmail.com>
2021-07-22 21:26:16 +0200

    [Game] Logging On (#287)
    Added the "Logged On" buff to characters that are logging on.

--- AAEmu.Game/Models/StaticValues/BuffsEnum.cs
+++ AAEmu.Game/Models/StaticValues/BuffsEnum.cs
@@ -5,6 +5,7 @@
         public const uint Untouchable = 545;
         public const uint FallStun = 1391; // From fall damage
         public const uint RemovalDebuff = 2250; // for houses
+        public const uint LoggedOn = 2423; // player is logging in
     }
  
 }
