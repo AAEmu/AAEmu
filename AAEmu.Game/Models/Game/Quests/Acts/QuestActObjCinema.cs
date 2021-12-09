68f900fc7248fe331fc10a9c8747af5a2ee911f1
And <78868624+And70@users.noreply.github.com>
2021-12-09 20:09:43 +0500

    Feat quest (#359)
    * Create QuestActObjAliase.cs
    
    * Create QuestActObjCinema.cs

---
+++ AAEmu.Game/Models/Game/Quests/Acts/QuestActObjCinema.cs
@@ -0,0 +1,18 @@
+using AAEmu.Game.Models.Game.Quests.Templates;
+using AAEmu.Game.Models.Game.Char;
+
+namespace AAEmu.Game.Models.Game.Quests.Acts
+{
+    public class QuestActObjCinema : QuestActTemplate
+    {
+        public uint CinemaId { get; set; }
+        public bool UseAlias { get; set; }
+        public uint QuestActObjAliasId { get; set; }
+
+        public override bool Use(Character character, Quest quest, int objective)
+        {
+            _log.Warn("QuestActObjCinema");
+            return false;
+        }
+    }
+}
