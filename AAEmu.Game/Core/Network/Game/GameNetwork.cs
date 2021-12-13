b398b27eb25b83bb19478170303f457389a95961
NLObP <38990417+NL0bP@users.noreply.github.com>
2021-12-13 10:51:55 +0300

    [Packets] Added CSRequestPermissionToPlayCinemaForDirectingMode with opcode 0x0D0;

--- AAEmu.Game/Core/Network/Game/GameNetwork.cs
+++ AAEmu.Game/Core/Network/Game/GameNetwork.cs
@@ -205,6 +205,7 @@ namespace AAEmu.Game.Core.Network.Game
             RegisterPacket(CSOffsets.CSUnbondDoodadPacket, 1, typeof(CSUnbondDoodadPacket));
             RegisterPacket(CSOffsets.CSCompletedCinemaPacket, 1, typeof(CSCompletedCinemaPacket));
             RegisterPacket(CSOffsets.CSStartedCinemaPacket, 1, typeof(CSStartedCinemaPacket));
+            RegisterPacket(CSOffsets.CSRequestPermissionToPlayCinemaForDirectingMode, 1, typeof(CSRequestPermissionToPlayCinemaForDirectingMode));
             //0xd0 CSRequestPermissionToPlayCinemaForDirectingMode
             //0xd1 CSEditorRemoveGimmickPacket
             //0xd2 CSEditorAddGimmickPacket
