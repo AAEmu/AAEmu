3881089660936c8435414cde988b7803c32062f2
ZeromusXYZ <zeromus.xyz@gmail.com>
2022-01-09 16:05:24 +0100

    [Game] WIP: Beauty Salon
    Added missing packets for Beauty Salon
    Implemented enter and exit salon

---
+++ AAEmu.Game/Core/Packets/C2G/CSApplyBeautySalonPacket.cs
@@ -0,0 +1,20 @@
+﻿using AAEmu.Commons.Network;
+using AAEmu.Game.Core.Network.Game;
+using AAEmu.Game.Core.Packets.G2C;
+
+namespace AAEmu.Game.Core.Packets.C2G
+{
+    public class CSApplyBeautySalonPacket : GamePacket
+    {
+        public CSApplyBeautySalonPacket() : base(CSOffsets.CSApplyBeautySalonPacket, 1)
+        {
+        }
+
+        public override void Read(PacketStream stream)
+        {
+            _log.Debug("ApplyBeautySalon");
+            Connection.ActiveChar.SendPacket(new SCToggleBeautyshopResponsePacket(0)); // close the Salon
+            Connection.ActiveChar.SendMessage("Salon is currently not working yet!");
+        }
+    }
+}
