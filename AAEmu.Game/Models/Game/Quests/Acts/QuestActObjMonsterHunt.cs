08ae8aa211f1e344b35e28b093870fd55b26cac6
NLObP <38990417+NL0bP@users.noreply.github.com>
2022-01-24 17:38:07 +0300

    [Game] WIP: исправление ошибок в системе квестов: - Добавил изменение награды за перевыполнение плана, если позволено квестом;

--- AAEmu.Game/Models/Game/Quests/Acts/QuestActObjMonsterHunt.cs
+++ AAEmu.Game/Models/Game/Quests/Acts/QuestActObjMonsterHunt.cs
@@ -22,12 +22,34 @@ namespace AAEmu.Game.Models.Game.Quests.Acts
             if (quest.Template.Score > 0) // Check if the quest use Template.Score or Count
             {
                 QuestActObjItemGather.HuntStatus = objective * Count;
-                return QuestActObjItemGather.HuntStatus + GatherStatus >= quest.Template.Score;
+                quest.OverCompletionPercent = QuestActObjItemGather.HuntStatus + GatherStatus;
+
+                if (quest.Template.LetItDone)
+                {
+                    if (quest.OverCompletionPercent >= quest.Template.Score * 3 / 5)
+                        quest.EarlyCompletion = true;
+
+                    if (quest.OverCompletionPercent > quest.Template.Score)
+                        quest.ExtraCompletion = true;
+                }
+
+                return quest.OverCompletionPercent >= quest.Template.Score;
             }
             else
             {
+                if (quest.Template.LetItDone)
+                {
+                    quest.OverCompletionPercent = objective * 100 / Count;
+
+                    if (quest.OverCompletionPercent >= 60)
+                        quest.EarlyCompletion = true;
+
+                    if (quest.OverCompletionPercent > 100)
+                        quest.ExtraCompletion = true;
+                }
                 return objective >= Count;
             }
+
         }
     }
 }
