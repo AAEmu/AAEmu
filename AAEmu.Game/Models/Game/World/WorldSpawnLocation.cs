31d42dd81e28011d4de4e1b0f0980d0359ccde40
ZeromusXYZ <zeromus.xyz@gmail.com>
2022-02-17 18:12:49 +0100

    [Game] Bugfix: Instance spawn locations
    Fix for the missing spawn locations for instances that was introduced when moving to ClientData.
    Created a new json for the missing data until we can find a better fix.

---
+++ AAEmu.Game/Models/Game/World/WorldSpawnLocation.cs
@@ -0,0 +1,15 @@
+﻿using AAEmu.Game.Models.Game.World.Transform;
+
+namespace AAEmu.Game.Models.Game.World
+{
+    public class WorldSpawnLocation
+    {
+        public string Name { get; set; }
+        public WorldSpawnPosition SpawnPosition { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " - " + SpawnPosition;
+        }
+    }
+}
