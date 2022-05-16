a2e5c487d3b007dc1b795759fb533d91cca42bcf
NLObP <38990417+NL0bP@users.noreply.github.com>
2022-05-16 23:47:43 +0300

    - Updated AI.Behaviors

--- AAEmu.Game/Models/Game/AI/v2/Behaviors/Common/HoldPositionBehavior.cs
+++ AAEmu.Game/Models/Game/AI/v2/Behaviors/Common/HoldPositionBehavior.cs
@@ -1,46 +1,17 @@
 ﻿using System;
-using System.Linq;
 
-using AAEmu.Commons.Utils;
-using AAEmu.Game.Core.Managers;
-using AAEmu.Game.Models.Game.Skills;
 using AAEmu.Game.Models.Game.Skills.Static;
-
-using NLog;
 
 namespace AAEmu.Game.Models.Game.AI.v2.Behaviors
 {
-    public class HoldPositionBehavior : BaseCombatBehavior
+    public class HoldPositionBehavior : Behavior
     {
-        private static Logger _log = LogManager.GetCurrentClassLogger();
-
         public override void Enter()
         {
         }
         public override void Tick(TimeSpan delta)
         {
-            PickSkillAndUseIt();
-        }
-
-        private void PickSkillAndUseIt()
-        {
-            if (!Ai.Owner.Template.Skills.ContainsKey(SkillUseConditionKind.InIdle)) { return; }
-            var skills = Ai.Owner.Template.Skills[SkillUseConditionKind.InIdle];
-            skills = skills
-                .Where(s => !Ai.Owner.Cooldowns.CheckCooldown(s.SkillId))
-                .Where(s =>
-                {
-                    var template = SkillManager.Instance.GetSkillTemplate(s.SkillId);
-                    return template is {TargetType: SkillTargetType.Self};
-                }).ToList();
-            if (skills.Count <= 0) { return; }
-            var npSkillId = skills[Rand.Next(skills.Count)].SkillId;
-            var skillTemplate = SkillManager.Instance.GetSkillTemplate(npSkillId);
-            if (Ai.Owner.Cooldowns.CheckCooldown(skillTemplate.Id)) { return; }
-            Ai.Owner.CurrentTarget = Ai.Owner; // target to self
-            var skill = new Skill(skillTemplate);
-            _log.Warn("PickSkillAndUseIt:UseSkill Owner.ObjId {0}, Owner.TemplateId {1}, SkillId {2}", Ai.Owner.ObjId, Ai.Owner.TemplateId, skillTemplate.Id);
-            UseSkill(skill, Ai.Owner.CurrentTarget, Ai.Owner.Template.BaseSkillDelay);
+            PickSkillAndUseIt(SkillUseConditionKind.InIdle, Ai.Owner);
         }
 
         public override void Exit()
--- AAEmu.Game/Models/Game/AI/v2/Framework/Behavior.cs
+++ AAEmu.Game/Models/Game/AI/v2/Framework/Behavior.cs
@@ -1,4 +1,15 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AAEmu.Commons.Utils;
+using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Models.Game.NPChar;
+using AAEmu.Game.Models.Game.Skills;
+using AAEmu.Game.Models.Game.Skills.Static;
+using AAEmu.Game.Models.Game.Units;
+
+using NLog;
 
 namespace AAEmu.Game.Models.Game.AI.v2
 {
@@ -21,26 +32,31 @@ namespace AAEmu.Game.Models.Game.AI.v2
         RunCommandSet,
         Spawning,
         Talk,
-        
+
         // Archer
         ArcherAttack,
-        
+
         // BigMonster
         BigMonsterAttack,
-        
+
         // Flytrap
         FlytrapAlert,
         FlytrapAttack,
-        
+
         // WildBoar
         WildBoarAttack
     }
-    
+
     /// <summary>
     /// Represents an AI state. Called as such because of naming in the game's files.
     /// </summary>
     public abstract class Behavior
     {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+
+        protected DateTime _delayEnd;
+        protected float _nextTimeToDelay;
+
         public NpcAi Ai { get; set; }
 
         public abstract void Enter();
@@ -51,10 +67,102 @@ namespace AAEmu.Game.Models.Game.AI.v2
         {
             return AddTransition(new Transition(on, kind));
         }
-        
+
         public Behavior AddTransition(Transition transition)
         {
             return Ai.AddTransition(this, transition);
         }
+
+        public void PickSkillAndUseIt(SkillUseConditionKind kind, BaseUnit target)
+        {
+            var targetDist = Ai.Owner.GetDistanceTo(target);
+            // Attack behavior probably only uses base skill ?
+            var skills = new List<NpcSkill>();
+            if (Ai.Owner.Template.Skills.ContainsKey(kind))
+                skills = Ai.Owner.Template.Skills[kind];
+            skills = skills
+                .Where(s => !Ai.Owner.Cooldowns.CheckCooldown(s.SkillId))
+                .Where(s =>
+                {
+                    var template = SkillManager.Instance.GetSkillTemplate(s.SkillId);
+                    return template != null && (targetDist >= template.MinRange && targetDist <= template.MaxRange || template.TargetType == SkillTargetType.Self);
+                }).ToList();
+
+            var pickedSkillId = (uint)Ai.Owner.Template.BaseSkillId;
+            if (skills.Count > 0)
+                pickedSkillId = skills[Rand.Next(skills.Count)].SkillId;
+
+            // Hackfix for Melee attack. Needs to look at the held weapon (if any) or default to 3m. 
+            if (pickedSkillId == 2 || targetDist > 4.0f)
+                return;
+            
+            var skillTemplate = SkillManager.Instance.GetSkillTemplate(pickedSkillId);
+            var skill = new Skill(skillTemplate);
+
+            if (!Ai.Owner.Cooldowns.CheckCooldown(skillTemplate.Id))
+            {
+                _log.Warn("PickSkillAndUseIt:UseSkill Owner.ObjId {0}, Owner.TemplateId {1}, SkillId {2}", Ai.Owner.ObjId, Ai.Owner.TemplateId, skillTemplate.Id);
+                var delay = 0f;
+                if (Ai.Owner.Template.BaseSkillDelay == 0)
+                {
+                    Ai.Owner.Cooldowns.AddCooldown(skillTemplate.Id, 95000); // in milliseconds
+                    delay = 0f; // in seconds
+                }
+                else
+                {
+                    delay = Ai.Owner.Template.BaseSkillDelay;
+                }
+                UseSkill(skill, target, delay);
+            }
+        }
+
+        // UseSkill (delay)
+        public void UseSkill(Skill skill, BaseUnit target, float delay = 0)
+        {
+            _nextTimeToDelay = delay;
+            var skillCaster = SkillCaster.GetByType(SkillCasterType.Unit);
+            skillCaster.ObjId = Ai.Owner.ObjId;
+
+            SkillCastTarget skillCastTarget;
+            switch (skill.Template.TargetType)
+            {
+                case SkillTargetType.Pos:
+                    var pos = Ai.Owner.Transform.World.Position;
+                    skillCastTarget = new SkillCastPositionTarget()
+                    {
+                        ObjId = Ai.Owner.ObjId,
+                        PosX = pos.X,
+                        PosY = pos.Y,
+                        PosZ = pos.Z,
+                        PosRot = Ai.Owner.Transform.World.ToRollPitchYawDegrees().Z // (float)MathUtil.ConvertDirectionToDegree(pos.RotationZ) //Is this rotation right?
+                    };
+                    break;
+                default:
+                    skillCastTarget = SkillCastTarget.GetByType(SkillCastTargetType.Unit);
+                    skillCastTarget.ObjId = target.ObjId;
+                    break;
+            }
+
+            var skillObject = SkillObject.GetByType(SkillObjectType.None);
+
+            skill.Callback = OnSkillEnded;
+            var result = skill.Use(Ai.Owner, skillCaster, skillCastTarget, skillObject);
+            if (result == SkillResult.Success)
+                Ai.Owner.LookTowards(target.Transform.World.Position);
+        }
+
+        public virtual void OnSkillEnded()
+        {
+            try
+            {
+                _delayEnd = DateTime.UtcNow.AddSeconds(_nextTimeToDelay);
+            }
+            catch
+            {
+
+            }
+        }
+
+        // Check if can pick a new skill (delay, already casting)
     }
 }
