612a49917aace73fec16ce003421a7dc900cb7d7
NLObP <38990417+NL0bP@users.noreply.github.com>
2022-05-23 21:49:06 +0300

    [Game] WIP: Quest bug fixes: - Fixed quest ID=2349б "Contacting the Shadowhawks", 5, "Arcum Iris Harani Main";

--- AAEmu.Game/Models/Game/Skills/Effects/SpecialEffects/Projectile.cs
+++ AAEmu.Game/Models/Game/Skills/Effects/SpecialEffects/Projectile.cs
@@ -21,6 +21,16 @@ namespace AAEmu.Game.Models.Game.Skills.Effects.SpecialEffects
         {
             // TODO ...
             if (caster is Character) { _log.Debug("Special effects: Projectile value1 {0}, value2 {1}, value3 {2}, value4 {3}", value1, value2, value3, value4); }
+
+            // TODO added for quest Id=2349
+            // find the item that was used for Buff and check it in the quests
+            if (caster is not Character character) { return; }
+            if (casterObj is not SkillItem skillItem) { return; }
+            var item = character.Inventory.GetItemById(skillItem.ItemId);
+            if (item is {Count: > 0})
+            {
+                character.Quests.OnItemUse(item);
+            }
         }
     }
 }
