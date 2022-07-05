bf9862761801f7afe050684ff12a6253020ac202
Roger Barreto <roger@rkti.com>
2022-07-05 10:33:52 +0100

    Fix build (#455)
    Co-authored-by: Rogerio Barreto <rbarreto@microsoft.com>

--- AAEmu.Game/Models/Game/Char/ICharacter.cs
+++ AAEmu.Game/Models/Game/Char/ICharacter.cs
@@ -1,8 +1,9 @@
 ﻿using System.Drawing;
+using AAEmu.Game.Models.Game.Units;
 
 namespace AAEmu.Game.Models.Game.Char
 {
-    public interface ICharacter
+    public interface ICharacter : IUnit
     {
         public void SendMessage(string message, params object[] parameters);
         public void SendMessage(Color color, string message, params object[] parameters);
