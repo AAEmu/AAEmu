0364ba96ecbf66fbc4618bca960b2bc42a1e860d
Roger Barreto <roger@rkti.com>
2022-07-09 19:28:06 +0100

    Added Serializer for Item References in Chat messages and Added ItemCmd using the same commands as the AddItem in the new Subcommand

---
+++ AAEmu.Game/Utils/Converters/ChatConverter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AAEmu.Game.Utils.Converters
+{
+    public class ChatConverter
+    {
+        private static byte[] Decrypt(string str)
+        {
+            var abc = new List<char>("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz*+".ToCharArray());
+            var size = (int)Math.Ceiling((double)str.Length * 6 / 8);
+            var result = new byte[size];
+
+            var res = new BitArray(size * 8);
+            res.SetAll(false);
+
+            var counter = 0;
+            var pos = 7;
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                var h = new BitArray(new byte[] { (byte)abc.IndexOf(str[i]) });
+                for (var i2 = 0; i2 < 6; i2++)
+                {
+                    res[pos--] = h[5 - i2];
+                    counter++;
+                    if (counter == 8)
+                    {
+                        pos += 16;
+                        counter = 0;
+                    }
+                }
+            }
+
+            res.CopyTo(result, 0);
+
+            return result;
+        }
+
+        private static string Encrypt(int a1, int a3, byte[] a4)
+        {
+            var v9 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz*+\0".ToCharArray();
+            int v10; // [sp+50h] [bp-8h]@1
+
+            var a2 = new char[a3];
+
+            var v4 = 0;
+            var v5 = 1;
+            var v6 = 2 * (a4[0] >> 7);
+            v10 = 8 * a1;
+            for (var i = 0; v5 < v10; ++v5)
+            {
+                v4 = i;
+                var v7 = (1 << 7 - (v5 & 7) & a4[v5 >> 3]) >> 7 - (v5 & 7) | v6;
+                if (v5 % 6 == 5)
+                {
+                    a2[i] = v9[v7];
+                    v4++;
+                    v6 = 0;
+                    i = v4;
+                    if (v4 >= a3) return new string(a2);
+                }
+                else v6 = 2 * v7;
+            }
+            if (v5 % 6 != 0)
+            {
+                v4++;
+                a2[v4 - 1] = v9[v6 >> 1 << 6 * v5 / 6 - (byte)v5 + 6];
+                if (v4 >= a3) return new string(a2);
+            }
+            else
+            {
+                a2[v4] = '\0';
+                return new string(a2);
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Creates a message encoded identifier for the item
+        /// </summary>
+        /// <param name="templateId">Template id of the item</param>
+        /// <param name="grade">The grade of the item</param>
+        /// <param name="durability">If not specified defaults to 100 to not show as red</param>
+        /// <returns></returns>
+        public static string ConvertAsChatMessageReference(uint templateId, byte grade, byte durability = 100)
+        {
+            
+            var templateIdBytes = BitConverter.GetBytes(templateId);
+
+            string encryptedItem = Encrypt(192, 384, new byte[]
+            {
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            templateIdBytes[0], templateIdBytes[1], //template id 
+            0x00, 0x00,
+            grade, // grade
+            0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+            durability, //durability
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+            });
+
+            return $"|i{templateId},{grade},{encryptedItem.Trim('\0')};";
+        }
+    }
+}
---
+++ AAEmu.Game/Utils/Scripts/SubCommands/Item/ItemAddSubCommand.cs
@@ -0,0 +1,119 @@
+﻿using System.Drawing;
+using System.Linq;
+using AAEmu.Game.Core.Managers;
+using AAEmu.Game.Core.Managers.World;
+using AAEmu.Game.Models.Game.Char;
+using AAEmu.Game.Models.Game.Items;
+using AAEmu.Game.Models.Game.Items.Actions;
+using AAEmu.Game.Utils.Converters;
+
+namespace AAEmu.Game.Utils.Scripts.SubCommands
+{
+    public class ItemAddSubCommand : SubCommandBase 
+    {
+        public ItemAddSubCommand()
+        {
+            Prefix = "[Item]";
+            Description = "Adds to self or a specified character or selected target an amount of a specific item template of a specific [grade].";
+            CallExample = "/item add (<charactername>||target||self) [amount=1] [grade=0]";
+        }
+
+        public override void PreExecute(ICharacter character, string triggerArgument, string[] args)
+        {
+            Character addTarget;
+            Character selfCharacter = (Character)character;
+            if (args.Length < 2)
+            {
+                SendHelpMessage(character);
+                return;
+            }
+
+            var firstArgument = args.First();
+            if (firstArgument == "target")
+            {
+                if ((selfCharacter.CurrentTarget is  null) || !(selfCharacter.CurrentTarget is Character))
+                {
+                    SendColorMessage(character, Color.Red, "Please select a valid character player");
+                    return;
+                }
+                addTarget = selfCharacter.CurrentTarget as Character;
+            }
+            else if (firstArgument == "self")
+            {
+                addTarget = selfCharacter;
+            }
+            else
+            {
+                Character player = WorldManager.Instance.GetCharacter(firstArgument);
+                if (player is null)
+                {
+                    SendColorMessage(character, Color.Red, $"Character player: {firstArgument} was not found.");
+                    return;
+                }
+                addTarget = player;
+            }
+
+            if (!uint.TryParse(args[1], out uint templateId))
+            {
+                SendColorMessage(character, Color.Red, "Item template id should be numeric");
+                return;
+            }
+
+            var itemTemplate = ItemManager.Instance.GetTemplate(templateId);
+            var itemAmount = 1; //Default amount 
+            if (itemTemplate is null)
+            {
+                SendColorMessage(character, Color.Red, $"Item template id {templateId} does not exist!|r");
+                return;
+            }
+
+            if (args.Length > 2 && (!int.TryParse(args[2], out itemAmount) || itemAmount <= 0))
+            {
+                SendColorMessage(character, Color.Red, "Item count id should be numeric and greater than 0");
+                return;
+            }
+
+            byte itemGrade = 0;
+            if (args.Length > 3) 
+            {
+                if (!byte.TryParse(args[3], out itemGrade))
+                {
+                    SendColorMessage(character, Color.Red, "Item grade should be numeric");
+                    return;
+                }
+
+                if (itemGrade > (byte)ItemGrade.Mythic || itemGrade < (byte)ItemGrade.Crude)
+                {
+                    SendColorMessage(character, Color.Red, "Item grade cannot be lower than {0} or exceed {1}!|r", (byte)ItemGrade.Crude, (byte)ItemGrade.Mythic);
+                    return;
+                }
+            }
+
+            if (ItemManager.Instance.IsAutoEquipTradePack(itemTemplate.Id))// .Category_Id == 133) || (itemTemplate.Category_Id == 122)) // Speciality Packs or Tradepacks
+            {
+                var currentBackpack = addTarget.Inventory.Equipment.GetItemBySlot((int)EquipmentItemSlot.Backpack);
+                if (currentBackpack != null)
+                {
+                    SendColorMessage(character, Color.Red, "No room on the backpack slot to place a tradepack!|r");
+                    return;
+                }
+                if (!addTarget.Inventory.Equipment.AcquireDefaultItem(ItemTaskType.Gm, templateId, itemAmount, itemGrade))
+                {
+                    SendColorMessage(character, Color.Red, "Tradepack could not be created!|r");
+                    return;
+                }
+            }
+            else if (!addTarget.Inventory.Bag.AcquireDefaultItem(ItemTaskType.Gm, templateId, itemAmount, itemGrade))
+            {
+                SendColorMessage(character, Color.Red, "Item could not be created!|r");
+                return;
+            }
+
+            if (selfCharacter.Id != addTarget.Id)
+            {
+                SendMessage(character, $"Added item {ChatConverter.ConvertAsChatMessageReference(templateId, itemGrade)} to {addTarget.Name}'s inventory");
+                SendMessage(addTarget, $"[GM] {selfCharacter.Name} added {ChatConverter.ConvertAsChatMessageReference(templateId, itemGrade)} to your inventory");
+            }
+        }
+    }
+}
