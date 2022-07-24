7cb6e3a836e756fb6b19ddeef76cec2a04a085ec
Vegax <vegax87@users.noreply.github.com>
2022-07-24 09:04:48 +0200

    /damage GM command is now based on health percentage (#451)
    * Damage command is now based on health percent
    
    I wondered why damage was hardcoded at only 100 HP, so I decided to tweak it
    
    usage: /damage <health percent> (between 1 and 99)
    
    * Update Damage.cs
    
    * Update Damage.cs
    
    I followed your instructions, it should be ok now
    
    * removed whitespace in Damage.cs
    
    removed whitespace at line 52

--- AAEmu.Game/Scripts/Commands/Damage.cs
+++ AAEmu.Game/Scripts/Commands/Damage.cs
@@ -17,17 +17,37 @@ namespace AAEmu.Game.Scripts.Commands
 
         public string GetCommandLineHelp()
         {
-            return "(target) or self";
+            return "(target) or self <damage> [%]";
         }
 
         public string GetCommandHelpText()
         {
-            return "Damages self";
+            return "Damages self or target based on raw damage or Hp percentage" +
+                   "Usage: /damage 9999   - Inflicts 9999 damage" +
+                   "or alternatively: /damage 80 %   - Inflicts 80% hp damage" +
+                   "Note: the percentage must be between 1 and 100";
         }
 
         public void Execute(Character character, string[] args)
         {
-            character.ReduceCurrentHp(character, 100);
+            if (args.Length == 0)
+            {
+                character.SendMessage("[Damage] " + CommandManager.CommandPrefix + "damage (self or target) <damage> [%]");
+                return;
+            }
+
+			if (!int.TryParse(args[0], out int damage))
+				damage = 1;
+				
+			if ((args.Length == 1) && (damage > 0))
+			{
+				character.ReduceCurrentHp(character, damage);
+			}
+
+            if ((args.Length == 2) && (args[1] == "%") && (damage > 0 && damage <= 100))
+            {
+               character.ReduceCurrentHp(character, (character.MaxHp * damage / 100));
+            }
         }
     }
 }
