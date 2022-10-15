3a628d09a4e1bae864ccc7b56c13d667bad33167
tilkinsc <ctilkins@mail.com>
2022-10-15 01:22:38 -0400

    More enum name changes

---
+++ AAEmu.Game/Models/StaticValues/ItemImplEnum.cs
@@ -0,0 +1,38 @@
+namespace AAEmu.Game.Models.StaticValues
+{
+    public enum ItemImplEnum
+    {
+        Misc = 0,
+        Weapon = 1,
+        Armor = 2,
+        Body = 3,
+        Bag = 4,
+        Housing = 5,
+        HousingDecoration = 6,
+        Tool = 7,
+        SummonSlave = 8,
+        SpawnDoodad = 9,
+        AcceptQuest = 10,
+        SummonMate = 11,
+        Recipe = 12,
+        Crafting = 13,
+        Portal = 14,
+        EnchantingGem = 15,
+        ReportCrime = 16,
+        LogicDoodad = 17,
+        HasUCC = 18,
+        OpenEmblemUi = 19,
+        Shipyard = 20,
+        Socket = 21,
+        Backpack = 22,
+        OpenPaper = 23,
+        Accessory = 24,
+        Treasure = 25,
+        MusicSheet = 26,
+        Dyeing = 27,
+        SlaveEquipment = 28,
+        GradeEnchantingSupport = 29,
+        MateArmor = 30,
+        Location = 31
+    }
+}
