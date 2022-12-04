03bf03ca0a7af4f7fa94b5782430bcb1e9ca348f
NL0bP <38990417+NL0bP@users.noreply.github.com>
2022-12-04 18:09:14 +0300

    [Game][SpecialEffects] Updated AddLaborPoints: - Flask 'ID=34071, Prime Worker's Compensation: 1,000 LP' now replenishes labor points

--- AAEmu.Game/Models/Game/Skills/Effects/SpecialEffects/AddLaborPower.cs
+++ AAEmu.Game/Models/Game/Skills/Effects/SpecialEffects/AddLaborPower.cs
@@ -20,7 +20,11 @@ namespace AAEmu.Game.Models.Game.Skills.Effects.SpecialEffects
             int value4)
         {
             // TODO ...
-            if (caster is Character) { _log.Debug("Special effects: AddLaborPower value1 {0}, value2 {1}, value3 {2}, value4 {3}", value1, value2, value3, value4); }
+            if (caster is Character character)
+            {
+                character.ChangeLabor((short)value1, 0);
+                _log.Debug("Special effects: AddLaborPower value1 {0}, value2 {1}, value3 {2}, value4 {3}", value1, value2, value3, value4);
+            }
         }
     }
 }
