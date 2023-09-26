af225d7ff980e623813f53008c0c5141b018ee35
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-09-26 22:04:44 +0100

    Removing unecessary pragma

--- AAEmu.Game/Services/WebApi/Controllers/WorldController.cs
+++ AAEmu.Game/Services/WebApi/Controllers/WorldController.cs
@@ -5,8 +5,6 @@ using NetCoreServer;
 
 namespace AAEmu.Game.Services.WebApi.Controllers;
 
-#pragma warning disable CA1822 // Mark members as static
-
 /// <summary>
 /// Character controller for the WebApi
 /// </summary>6
