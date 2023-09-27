c03c90c79d4a4f850c6b8076230988b88a9d0085
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-09-27 06:15:03 +0100

    WebApi Improvements + WorldController (#733)
    * Added Route Mapping + AutoDiscovery of routes with Controller concept
    
    * Passing down the HttpRequest to the handlers for more advanced scenarios
    
    * Changes
    
    * Removing the snicky Config
    
    * Removing unecessary pragma

---
+++ AAEmu.Game/Services/WebApi/Controllers/WorldController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using AAEmu.Game.Core.Managers.World;
+using AAEmu.Game.Services.WebApi.Models;
+using NetCoreServer;
+
+namespace AAEmu.Game.Services.WebApi.Controllers;
+
+/// <summary>
+/// Character controller for the WebApi
+/// </summary>6
+internal class WorldController : BaseController
+{
+    [WebApiGet("/world/logged-characters")]
+    public HttpResponse GetCharacter(HttpRequest request)
+    {
+        var loggedCharacters = WorldManager.Instance.GetAllCharacters().Select(x => new CharacterModel(x.Id, x.Name));
+        return OkJson(loggedCharacters);
+    }
+}
