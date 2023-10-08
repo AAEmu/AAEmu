88c4aa162e2a5b12c82c2a336c1120465bf743b7
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-10-08 15:35:23 +0100

    Commands Controller - Phase 1 (#750)
    * Improving route resolution, parameter discovery for regex/pattern matching
    
    * Config.json is not required when User Secrets are defined
    
    * Small fix

---
+++ AAEmu.Game/Services/WebApi/RouteDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Reflection;
+
+namespace AAEmu.Game.Services.WebApi;
+
+public record RouteDefinition
+{
+    public string Key { get; }
+    public string Path { get; }
+    public HttpMethod Method { get; }
+    public MethodInfo TargetMethod { get; }
+
+    public RouteDefinition(string path, HttpMethod httpMethod, MethodInfo targetMethod)
+    {
+        Path = path;
+        Method = httpMethod;
+        TargetMethod = targetMethod;
+        Key = GetRouteKey(path, httpMethod);
+    }
+
+    public static string GetRouteKey(string path, HttpMethod method)
+    {
+        return string.Concat(method ?? HttpMethod.Get, ':', path);
+    }
+}
