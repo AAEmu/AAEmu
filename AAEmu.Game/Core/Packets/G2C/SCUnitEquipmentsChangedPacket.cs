ccc55a0fbbac9e3116a577228983cf814c3742f6
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-04-03 18:41:36 +0300

    [Game] Fixed putting items on pets; - when removing an item, it sometimes changes to the character’s clothes; #22

--- AAEmu.Game/Core/Packets/G2C/SCUnitEquipmentsChangedPacket.cs
+++ AAEmu.Game/Core/Packets/G2C/SCUnitEquipmentsChangedPacket.cs
@@ -1,5 +1,6 @@
 ﻿using AAEmu.Commons.Network;
 using AAEmu.Game.Core.Network.Game;
+using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.Items;
 
 namespace AAEmu.Game.Core.Packets.G2C;
@@ -18,11 +19,14 @@ public class SCUnitEquipmentsChangedPacket : GamePacket
     public SCUnitEquipmentsChangedPacket(uint objectId, byte slot, Item item) : base(SCOffsets.SCUnitEquipmentsChangedPacket, 5)
     {
         _objectId = objectId;
-        _items = new[] { (slot, item) };
+        _items = [(slot, item)];
     }
 
     public override PacketStream Write(PacketStream stream)
     {
+        var index = 0;
+        var ItemFlags = 0;
+
         stream.WriteBc(_objectId);
         stream.Write((byte)_items.Length); // TODO max 28
         foreach (var (slot, item) in _items)
@@ -31,8 +35,15 @@ public class SCUnitEquipmentsChangedPacket : GamePacket
             if (item == null)
                 stream.Write(0);
             else
+            {
                 stream.Write(item);
+                var v15 = (int)item.ItemFlags << index;
+                ++index;
+                ItemFlags |= v15;
+            }
         }
+
+        stream.Write(ItemFlags); // added for 3.0.3.0
 
         return stream;
     }
