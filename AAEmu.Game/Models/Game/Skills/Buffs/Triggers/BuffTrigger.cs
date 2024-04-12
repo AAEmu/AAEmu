9dc365dd229c09f1f1b99f4b87e9c37c046d7a1b
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-04-12 10:48:27 +0300

    [Game] fixed BuffTrigger - doodads can also be buff casters;

--- AAEmu.Game/Models/Game/Skills/Buffs/Triggers/BuffTrigger.cs
+++ AAEmu.Game/Models/Game/Skills/Buffs/Triggers/BuffTrigger.cs
@@ -20,12 +20,41 @@ public class BuffTrigger
         Logger.Trace("Buff[{0}] {1} executed. Applying {2}[{3}]!", _buff?.Template?.BuffId, GetType().Name, Template.Effect.GetType().Name, Template.Effect.Id);
         //Template.Effect.Apply()
 
-        if (_owner is not Unit)
+        if (_owner is not Unit) // if doodads
         {
-            Logger.Warn("Owner is not a Unit");
+            Logger.Warn("Owner is a Doodad");
+            var target0 = _buff?.Owner;
+            var source0 = _buff?.Owner;
+
+            if (Template.UseOriginalSource)
+            {
+                source0 = _buff?.Caster;
+            }
+
+            if (Template.EffectOnSource)
+            {
+                target0 = source0;
+            }
+
+            if (Template.TargetBuffTagId != 0)
+            {
+                if (target0 != null && !target0.Buffs.CheckBuffTag(Template.TargetBuffTagId))
+                    return;
+            }
+            if (Template.TargetNoBuffTagId != 0)
+            {
+                if (target0 != null && target0.Buffs.CheckBuffTag(Template.TargetNoBuffTagId))
+                    return;
+            }
+
+            if (target0 == null) { return; }
+            Template.Effect.Apply(source0, new SkillCasterUnit(_owner.ObjId), target0, new SkillCastUnitTarget(target0.ObjId), new CastBuff(_buff),
+                new EffectSource(_buff?.Skill), // TODO : EffectSource Type trigger 
+                null, DateTime.UtcNow);
             return;
         }
 
+        Logger.Warn("Owner is a Unit");
         var target = _buff?.Owner;
         var source = (Unit)_buff?.Owner;
 
