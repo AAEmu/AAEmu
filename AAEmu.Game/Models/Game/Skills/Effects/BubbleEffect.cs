99f41dd5ff584aa44387e6f48ba39262c752ea7a
ZeromusXYZ <zeromus.xyz@gmail.com>
2024-04-21 09:54:15 +0200

    [Game] Added Bubble Chat and Skill Effects weights
    - Implemented the BubbleChat Skill Effect
    - Implemented the Weight setting of Skill Effects

--- AAEmu.Game/Models/Game/Skills/Effects/BubbleEffect.cs
+++ AAEmu.Game/Models/Game/Skills/Effects/BubbleEffect.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-
 using AAEmu.Game.Core.Packets;
+using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.Models.Game.Skills.Templates;
 using AAEmu.Game.Models.Game.Units;
 
@@ -16,6 +16,9 @@ public class BubbleEffect : EffectTemplate
         CastAction castObj, EffectSource source, SkillObject skillObject, DateTime time,
         CompressedGamePackets packetBuilder = null)
     {
-        Logger.Debug("BubbleEffect");
+        // var sampleText = LocalizationManager.Instance.Get("bubble_effects", "speech", Id, "");
+        Logger.Trace($"BubbleEffect, Id {Id}, KindId {KindId}, ObjId {targetObj.ObjId}"); //, Text {sampleText}");
+        // TODO: Verify if this can be a normal Broadcast, or if it should only go towards the caster and/or target
+        target?.BroadcastPacket(new SCChatBubblePacket(targetObj.ObjId, (byte)KindId, 2, Id, ""), true);
     }
 }
