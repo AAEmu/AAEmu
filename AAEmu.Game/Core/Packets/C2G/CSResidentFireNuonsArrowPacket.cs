394099f05f8af9d502dd3b38b666d2df8da6862e
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-05-23 15:51:44 +0300

    [Game] WIP: Regional Community House: - Added templates for packaets; - Added templates for func and phase func;

---
+++ AAEmu.Game/Core/Packets/C2G/CSResidentFireNuonsArrowPacket.cs
@@ -0,0 +1,17 @@
+﻿using AAEmu.Commons.Network;
+using AAEmu.Game.Core.Network.Game;
+
+namespace AAEmu.Game.Core.Packets.C2G
+{
+    public class CSResidentFireNuonsArrowPacket : GamePacket
+    {
+        public CSResidentFireNuonsArrowPacket() : base(CSOffsets.CSResidentFireNuonsArrowPacket, 5)
+        {
+        }
+
+        public override void Read(PacketStream stream)
+        {
+            Logger.Debug("CSResidentFireNuonsArrowPacket");
+        }
+    }
+}
