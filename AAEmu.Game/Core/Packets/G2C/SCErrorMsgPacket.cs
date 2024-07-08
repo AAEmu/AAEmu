50cc604b5f8bf3904277fbd743f3791f57b0a593
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-07-08 21:13:54 +0300

    [Game] Crafting bug fixed: (#997)
    - now packs are crafted as expected, no more than one.
    - ordinary items are crafted into inventory.
    - it turned out that you need to send the correct SCErrorMsgPacket to cancel crafting.

--- AAEmu.Game/Core/Packets/G2C/SCErrorMsgPacket.cs
+++ AAEmu.Game/Core/Packets/G2C/SCErrorMsgPacket.cs
@@ -6,21 +6,31 @@ namespace AAEmu.Game.Core.Packets.G2C;
 
 public class SCErrorMsgPacket : GamePacket
 {
-    private readonly short _errorMessage;
+    private readonly short _errorMessage1;
+    private readonly short _errorMessage2;
     private readonly uint _type;
     private readonly bool _isNotify;
 
     public SCErrorMsgPacket(ErrorMessageType errorMessage, uint type, bool isNotify) : base(SCOffsets.SCErrorMsgPacket, 1)
     {
-        _errorMessage = (short)errorMessage; // TODO - NEED TO FIND ALL 350 IDS
+        _errorMessage1 = (short)errorMessage; // TODO - NEED TO FIND ALL 350 IDS
+        _errorMessage2 = _errorMessage1;
+        _type = type;
+        _isNotify = isNotify;
+    }
+
+    public SCErrorMsgPacket(ErrorMessageType errorMessage1, ErrorMessageType errorMessage2, uint type, bool isNotify) : base(SCOffsets.SCErrorMsgPacket, 1)
+    {
+        _errorMessage1 = (short)errorMessage1;
+        _errorMessage2 = (short)errorMessage2;
         _type = type;
         _isNotify = isNotify;
     }
 
     public override PacketStream Write(PacketStream stream)
     {
-        stream.Write(_errorMessage);
-        stream.Write(_errorMessage);
+        stream.Write(_errorMessage1);
+        stream.Write(_errorMessage2);
         stream.Write(_type);
         stream.Write(_isNotify);
         return stream;
