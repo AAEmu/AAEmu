002e34999325c974a6d983d57f0574f161f3d22f
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-07-09 00:12:12 +0300

    [Game] Crafting bug fixed: - simplified the glider check.
    [Game] Fixed an error in raising the pack:
    - added a check similar to the check in crafting for the presence of a backpack.

--- AAEmu.Game/Models/Game/World/Interactions/RecoverItem.cs
+++ AAEmu.Game/Models/Game/World/Interactions/RecoverItem.cs
@@ -1,7 +1,9 @@
 ﻿using AAEmu.Game.Core.Managers.UnitManagers;
+using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.DoodadObj;
 using AAEmu.Game.Models.Game.DoodadObj.Funcs;
 using AAEmu.Game.Models.Game.DoodadObj.Templates;
+using AAEmu.Game.Models.Game.Items;
 using AAEmu.Game.Models.Game.Skills;
 using AAEmu.Game.Models.Game.Units;
 
@@ -12,7 +14,10 @@ public class RecoverItem : IWorldInteraction
     public void Execute(BaseUnit caster, SkillCaster casterType, BaseUnit target, SkillCastTarget targetType,
         uint skillId, uint doodadId, DoodadFuncTemplate objectFunc = null)
     {
-        if (target is Doodad doodad && doodad.AllowRemoval())
+        // check if you are equipped with a backpack or glider
+        var hasBackPack = !((Character)caster).Inventory.CanReplaceGliderInBackpackSlot();
+
+        if (target is Doodad doodad && doodad.AllowRemoval() && !hasBackPack)
         {
             // Get Funcs for current doodad phase
             var funcs = DoodadManager.Instance.GetFuncsForGroup(doodad.FuncGroupId);
