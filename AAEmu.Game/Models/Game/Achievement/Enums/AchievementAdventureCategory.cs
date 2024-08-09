8b57d915882436eb19ae3ce50969e138fef54f20
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-08-09 16:28:40 +0300

    [Game] WIP: Let's start adding Achievements.

---
+++ AAEmu.Game/Models/Game/Achievement/Enums/AchievementAdventureCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace AAEmu.Game.Models.Game.Achievement.Enums;
+
+public enum AchievementAdventureCategory
+{
+    Invalid = 0,
+    Quest = 1,
+    Indun = 2,
+    Get = 3
+}
---
+++ AAEmu.Game/Models/Game/Achievement/Enums/AchievementSeasonCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace AAEmu.Game.Models.Game.Achievement.Enums;
+
+public enum AchievementSeasonCategory
+{
+    Invalid = 0,
+    Spring = 1,
+    Summer = 2,
+    Autumn = 3,
+    Winter = 4
+}
