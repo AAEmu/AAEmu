8133ed144220f35e6486652bb08f4aeba9142144
Hermano Cabral <hermanocabral@hermanocabral.com>
2024-08-19 22:53:19 +0700

    [All] Update Docker scripts

--- Scripts/docker-install-local.sh
+++ Scripts/docker-install-local.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-clear
-
 # Switch to the root folder and clean everything old
 echo -e "Switching folder to root folder (AAEmu/)"
 cd ..
@@ -17,9 +15,7 @@ while true; do
     if [[ "$answer" == "Y" ]]; then
         echo "You chose to delete everything to start a fresh installation. Proceeding..."  
         docker compose down   
-        rm -f .env
         rm -rf .server_files
-        git checkout .env
         break
     elif [[ "$answer" == "N" ]]; then
         echo "You chose not to proceed with a fresh installation. Aborting..."
@@ -42,38 +38,38 @@ mkdir -p .server_files/AAEmu.Game
 mkdir -p .server_files/AAEmu.Game/Data
 mkdir -p .server_files/AAEmu.Game/ClientData
 mkdir -p .server_files/AAEmu.Game/Configurations
-echo -e "Done"; sleep 1
+echo -e "Done"
 
 # Prepare the configuration files
 # Generating a strong Database Password
 echo -e "Generating strong SQL Database password..."
 DB_PASSWORD=$(openssl rand -base64 32)
-echo -e "Done"; sleep 1
+echo -e "Done"
 # Generating a strong secret between login server and game server
 echo -e "Generating strong Secret Key between Login Server and Game Server..."
 SECRET_KEY=$(openssl rand -base64 32)
-echo -e "Done"; sleep 1
+echo -e "Done"
 
 # Copying the necessary files in place
 echo -e "Copying file AAEmu/.env.example to Copying file AAEmu/.env if not already present."
 cp -n .env.example .env
-echo -e "Done"; sleep 1
+echo -e "Done"
 
 echo -e "Copying file AAEmu/AAEmu.Login/ExampleConfig.json to AAEmu/.server_files/AAEmu.Login/Config.json if not already present."
 cp -n AAEmu.Login/ExampleConfig.json .server_files/AAEmu.Login/Config.json
-echo -e "Done"; sleep 1
+echo -e "Done"
 
 echo -e "Copying file AAEmu/AAEmu.Game/ExampleConfig.json to AAEmu/.server_files/AAEmu.Game/Config.json if not already present."
 cp -n AAEmu.Game/ExampleConfig.json .server_files/AAEmu.Game/Config.json
-echo -e "Done"; sleep 1
+echo -e "Done"
 
 echo -e "Copying folder AAEmu/AAEmu.Game/Configurations to AAEmu/.server_files/AAEmu.Game/Configurations if not already present."
 cp -n -r AAEmu.Game/Configurations .server_files/AAEmu.Game/
-echo -e "Done"; sleep 1
+echo -e "Done"
 
 echo -e "Copying folder AAEmu/AAEmu.Game/Data to AAEmu/.server_files/AAEmu.Game/Data if not already present."
 cp -n -r AAEmu.Game/Data .server_files/AAEmu.Game/
-echo -e "Done"; sleep 1
+echo -e "Done"
 
 # Now configuring files in place
 # Configuring Login Server Config.json
@@ -83,7 +79,7 @@ sed -i "s|%db_host%|db|" .server_files/AAEmu.Login/Config.json
 sed -i "s|%db_port%|3306|" .server_files/AAEmu.Login/Config.json
 sed -i "s|%db_user%|root|" .server_files/AAEmu.Login/Config.json
 sed -i "s|%db_password%|$DB_PASSWORD|" .server_files/AAEmu.Login/Config.json
-echo -e "Done"; sleep 1
+echo -e "Done"
 # Configuring Game Server Config.json
 echo -e "Configuring Config.json for the Game Server..."
 sed -i "s|\"SecretKey\": \"test\"|\"SecretKey\": \"$SECRET_KEY\"|" .server_files/AAEmu.Game/Config.json
@@ -93,14 +89,13 @@ sed -i "s|%db_user%|root|" .server_files/AAEmu.Game/Config.json
 sed -i "s|%db_password%|$DB_PASSWORD|" .server_files/AAEmu.Game/Config.json
 sed -i "s|%login_host%|login|" .server_files/AAEmu.Game/Config.json
 sed -i "s|%login_port%|1234|" .server_files/AAEmu.Game/Config.json
-echo -e "Done"; sleep 1
+echo -e "Done"
+# Configuring .env
+sed -i "s|^DB_PASSWORD=.*|DB_PASSWORD=$DB_PASSWORD|" .env
 
 # Build the Dockerfile using the Compose File
-sed -i "s|^DB_PASSWORD=.*|DB_PASSWORD=$DB_PASSWORD|" .env
 docker compose build --no-cache
-echo -e "Done"; sleep 1
-
-clear
+echo -e "Done"
 
 echo -e "Theses are the generated password, keep them safe, they will not be showed again except in config files :"
 echo "DB Password: $DB_PASSWORD"
@@ -114,4 +109,4 @@ echo -e ""
 echo -e "Launch by running 'docker compose up -d' in the root of AAEmu and connect to 127.0.0.1 in AAEmu.Launcher"
 echo -e "You can stop the server by issuing the command 'docker compose down' in the root of AAEmu"
 echo -e "If you want a development environment (with quick reload and auto-rebuild), use the command docker compose watch or docker compose up --watch and DO NOT CLOSE the terminal"
-read -p "Press any key to finish..."
+echo -e "Docker setup done"
--- Scripts/docker-update-local.sh
+++ Scripts/docker-update-local.sh
@@ -1,26 +1,20 @@
-# Clear the screen
-clear
-
 # Switch to the root folder
 echo -e "Switching folder to root folder (AAEmu\)"
 cd ..
 echo -e "Done"
-sleep 1
 
 echo -e "Shutting all containers and Updating AAEmu..."
 docker compose down
 git pull
 echo -e "Done"
-sleep 1
 
 # Build the Dockerfile
 echo -e "Rebuilding Docker Containers..."
 docker compose build --no-cache
 echo -e "Done"
-sleep 1
 
 # The End
 echo -e "Launch by running 'docker compose up -d' in the root of AAEmu and connect to 127.0.0.1 in AAEmu.Launcher"
 echo -e "You can stop the server by issuing the command 'docker compose down' in the root of AAEmu"
 echo -e "If you want a development environment (with quick reload and auto-rebuild), use the command docker compose watch or docker compose up --watch and DO NOT CLOSE the terminal"
-read -p "Press Enter to finish..."
+echo -e "Update done"
