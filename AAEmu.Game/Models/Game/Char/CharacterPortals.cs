e9f3e2c350f94ee1d283e31f107827e493b6dd73
Yanlongli <ahlyl94@gmail.com>
2024-08-28 13:51:32 +0800

    [Game] Fix the issue where renaming a private teleportation point will result in the creation of a new teleportation point (#1092)

--- AAEmu.Game/Models/Game/Char/CharacterPortals.cs
+++ AAEmu.Game/Models/Game/Char/CharacterPortals.cs
@@ -100,6 +100,17 @@ public class CharacterPortals
         Owner.SendPacket(new SCCharacterPortalsPacket(new[] { newPortal }));
     }
 
+    public bool ChangePrivatePortalName(uint id, string name)
+    {
+        if (PrivatePortals.TryGetValue((uint)id, out var privatePortal))
+        {
+            privatePortal.Name = name;
+            Owner.SendPacket(new SCPortalInfoSavedPacket(privatePortal));
+            return true;
+        }
+
+        return false;
+    }
     public void Send()
     {
         if (PrivatePortals.Count > 0)
