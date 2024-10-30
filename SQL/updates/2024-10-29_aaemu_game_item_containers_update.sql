1271ed45132363d7addab7fb527154b38bf88cd1
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-10-31 01:01:44 +0300

    [SQL] Patches to update the item_containers and items tables if we have players removed: 2024-10-29_aaemu_game_item_containers_update.sql, 2024-10-29_aaemu_game_items_update.sql; added: 2024-10-30_aaemu_game_item_containers_update.sql, 2024-10-30_aaemu_game_items_update.sql;

--- SQL/updates/2024-10-29_aaemu_game_item_containers_update.sql
+++
@@ -1,8 +0,0 @@
--- --------------------------------------------
--- Update item_containers table with
--- --------------------------------------------
-ALTER TABLE item_containers
-	DROP COLUMN slot_type;
-ALTER TABLE item_containers
-	ADD COLUMN `slot_type` INT(11) NOT NULL DEFAULT '0' COMMENT 'Internal Container Type' AFTER `container_type`;
-	
