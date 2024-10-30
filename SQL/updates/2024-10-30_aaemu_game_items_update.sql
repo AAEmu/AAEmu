9e1155de7195abdf03aea0f71cff40797b1bf97e
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-10-31 00:36:07 +0300

    Patches to update the `item_containers` and `items` tables if we have players.

---
+++ SQL/updates/2024-10-30_aaemu_game_items_update.sql
@@ -0,0 +1,33 @@
+-- 1. Add a temporary field old_slot_type
+ALTER TABLE `items` 
+ADD COLUMN `old_slot_type` ENUM('Equipment', 'Inventory', 'Bank', 'Trade', 'Mail', 'System', 'EquipmentMate') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci 
+DEFAULT NULL COMMENT 'Old slot type for conversion';
+
+-- 2. Copy values ​​from slot_type to old_slot_type
+UPDATE `items` 
+SET `old_slot_type` = `slot_type`;
+
+-- 3. Delete the old slot_type
+ALTER TABLE `items` 
+DROP COLUMN `slot_type`;
+
+-- 4. Add a new slot_type with type int
+ALTER TABLE `items` 
+ADD COLUMN `slot_type` INT NOT NULL DEFAULT 0 COMMENT 'Internal Container Type';
+
+-- 5. Update slot_type based on old_slot_type
+UPDATE `items` 
+SET `slot_type` = CASE 
+    WHEN `old_slot_type` = 'Equipment' THEN 1
+    WHEN `old_slot_type` = 'Inventory' THEN 2
+    WHEN `old_slot_type` = 'Bank' THEN 3
+    WHEN `old_slot_type` = 'Trade' THEN 4
+    WHEN `old_slot_type` = 'Mail' THEN 5
+    WHEN `old_slot_type` = 'System' THEN 255
+    WHEN `old_slot_type` = 'EquipmentMate' THEN 252
+    ELSE 0  -- Set to 0 for all other values
+END;
+
+-- 6. Delete the temporary field old_slot_type if it is no longer needed
+-- ALTER TABLE `items` 
+-- DROP COLUMN `old_slot_type`;
