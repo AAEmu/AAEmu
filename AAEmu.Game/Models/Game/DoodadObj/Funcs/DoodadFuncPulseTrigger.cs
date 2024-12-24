228a2f8bb96676fb6da6f5fe95c315a88d6c0c84
NLObP <38990417+NL0bP@users.noreply.github.com>
2024-12-24 08:43:47 +0300

    [Game] The functionality of pulse triggers has been restored.

--- AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncPulseTrigger.cs
+++ AAEmu.Game/Models/Game/DoodadObj/Funcs/DoodadFuncPulseTrigger.cs
@@ -1,4 +1,7 @@
-﻿using AAEmu.Game.Models.Game.DoodadObj.Templates;
+﻿using System.Linq;
+
+using AAEmu.Game.Models.Game.Char;
+using AAEmu.Game.Models.Game.DoodadObj.Templates;
 using AAEmu.Game.Models.Game.Units;
 
 namespace AAEmu.Game.Models.Game.DoodadObj.Funcs;
@@ -10,10 +13,11 @@ public class DoodadFuncPulseTrigger : DoodadPhaseFuncTemplate
 
     public override bool Use(BaseUnit caster, Doodad owner)
     {
-        return true;
-        // TODO: Currently disabled because of lacking functionality of Pulse Triggers
+        if (caster is not Character)
+        {
+            return true;
+        }
 
-        /*
         // Grab the calling PhaseFunc
         var thisPhaseFunc = owner.CurrentPhaseFuncs.FirstOrDefault(x => x.FuncId == Id);
         if (thisPhaseFunc == null)
@@ -21,7 +25,7 @@ public class DoodadFuncPulseTrigger : DoodadPhaseFuncTemplate
             Logger.Warn($"DoodadFuncPulseTrigger Flag={Flag}, NextPhase={NextPhase} was not triggered from a DoodadFuncPulseTrigger");
             return false; // Fail check as there seems to be a mismatch
         }
-        
+
         Logger.Debug($"DoodadFuncPulseTrigger Flag={Flag}, NextPhase={NextPhase}, PulseTriggered={thisPhaseFunc.PulseTriggered}");
 
         if (Flag && !thisPhaseFunc.PulseTriggered)
@@ -33,6 +37,6 @@ public class DoodadFuncPulseTrigger : DoodadPhaseFuncTemplate
         }
 
         return false;
-        */
+
     }
 }
