88abb06f1d6ab81aaa6e3dca2dfaa3393e84d41c
ZeromusXYZ <zeromus.xyz@gmail.com>
2025-01-13 13:44:00 +0100

    [Client] Added fix for quest Elf Main quest 3889
    - Typo in the comments of the SQL

--- SQL/patches/compact/2025-01-13 - Fix Quest - Closing the Gate (3889).sql
+++ SQL/patches/compact/2025-01-13 - Fix Quest - Closing the Gate (3889).sql
@@ -1,5 +1,5 @@
 -- Author: ZeromusXYZ - 2025/01/13
 -- Fix for Elf Main quest "Closing the Gate ( 3889 )"
--- Original value1 was 4280
+-- Original quest_id was 4280
 -- Fixes what SphereQuest needs to be check
 UPDATE "main"."sphere_quests" SET "quest_id"=3889 WHERE "id"='789'
