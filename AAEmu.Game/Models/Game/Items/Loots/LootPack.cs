e5a13e3a8e0707eedeefd6aeecd918eaf2e608ac
NLObP <38990417+NL0bP@users.noreply.github.com>
2025-02-14 00:33:22 +0300

    updated;

--- AAEmu.Game/Models/Game/Items/Loots/LootPack.cs
+++ AAEmu.Game/Models/Game/Items/Loots/LootPack.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using AAEmu.Commons.Utils;
 using AAEmu.Game.Core.Managers;
 using AAEmu.Game.Core.Managers.World;
