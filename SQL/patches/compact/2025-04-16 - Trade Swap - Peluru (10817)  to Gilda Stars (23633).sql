beec223f08ea9364dd50db4df0d0598934bb11b9
Arska-cloud <arnaudeoliveira@gmail.com>
2025-04-16 12:52:20 +0200

    Created patch for Gilda Stars in Freedich Island

---
+++ SQL/patches/compact/2025-04-16 - Trade Swap - Peluru (10817)  to Gilda Stars (23633).sql
@@ -0,0 +1,5 @@
+-- Author: Pensive Ire - 2025/04/16
+-- Change of trade item at Freedich Island to Gilda Stars
+-- NPC ID is 10817 (Peluru)
+-- Gilda Star ID is 23633
+UPDATE "npcs" SET "specialty_coin_id"=23633 WHERE "id"="10817"
