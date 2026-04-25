ea3bea05795627d0d98c72f2a4bc84414f836f89
RagoroAA <ragoro@mail.com>
2026-04-25 16:10:33 +0300

    fix(game): import ErrorMessageType for CSStartSkill

--- AAEmu.Game/Core/Packets/C2G/CSStartSkillPacket.cs
+++ AAEmu.Game/Core/Packets/C2G/CSStartSkillPacket.cs
@@ -4,6 +4,7 @@ using AAEmu.Game.Core.Managers.World;
 using AAEmu.Game.Core.Network.Game;
 using AAEmu.Game.Core.Packets.G2C;
 using AAEmu.Game.GameData;
+using AAEmu.Game.Models.Game;
 using AAEmu.Game.Models.Game.Char;
 using AAEmu.Game.Models.Game.DoodadObj.Static;
 using AAEmu.Game.Models.Game.Items.Templates;
