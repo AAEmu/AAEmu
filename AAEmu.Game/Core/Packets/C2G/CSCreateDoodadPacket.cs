7ba4ab14db00ef0eeba94c916281dce25d149131
ZeromusXYZ <zeromus.xyz@gmail.com>
2026-07-18 19:41:33 +0200

    feat(game): Labor cost planting outside owned land
    `CSCreateDoodadPacket` now verifies several conditions.
    - Checks if the referenced item actually exists first.
    - Finds the related skill to calculate the labor costs for placing this item.
    - If in a public farm area, validates the type, and reduces cost to 0.
    - If on player owned land, verify permission and set labor cost to 0.
    - Verify required labor before placing.

--- AAEmu.Game/Core/Packets/C2G/CSCreateDoodadPacket.cs
+++ AAEmu.Game/Core/Packets/C2G/CSCreateDoodadPacket.cs
@@ -24,22 +24,68 @@ public class CSCreateDoodadPacket() : GamePacket(CSOffsets.CSCreateDoodadPacket,
         Logger.Warn($"CreateDoodad, Id: {id}, X: {x}, Y: {y}, Z: {z}, zRot: {zRot}  ItemId: {itemId}");
 
         var pos = new Vector3(x, y, z);
+        var laborCost = 0;
+
+        // Verify actual item
+        var item = Connection.ActiveChar.Inventory.GetItemById(itemId);
+        if (item is null)
+        {
+            Logger.Warn($"CreateDoodad, {Connection.ActiveChar.Name} provided an invalid ItemId: {itemId}");
+            return;
+        }
+
+        // Get cost from related skill
+        var useSkill = SkillManager.Instance.GetSkillTemplate(item.Template.UseSkillId);
+        if (useSkill is not null)
+        {
+            laborCost = useSkill.ConsumeLaborPower;
+        }
+
         var inPublicFarm = PublicFarmManager.Instance.InPublicFarm(Connection.ActiveChar.ParentWorld.Template, pos);
         var farmType = inPublicFarm
             ? PublicFarmManager.Instance.GetFarmType(Connection.ActiveChar.ParentWorld, pos)
             : FarmType.Invalid;
+
+        // Validate public farm
         if (farmType != FarmType.Invalid)
         {
             if (!PublicFarmManager.Instance.CanPlace(Connection.ActiveChar, farmType, id))
             {
                 // Invalid public farm
-                Logger.Warn($"CreateDoodad, ItemId: {itemId}, FarmType: {farmType}");
+                Logger.Warn($"CreateDoodad, ItemId: {itemId}, Invalid FarmType: {farmType}");
+                return;
+            }
+            laborCost = 0;
+        }
+
+        // Check if labor cost can be skipped by planting on "owned" land (or with permission)
+        // Also blocks if not owned
+        var house = HousingManager.Instance.GetHouseAtLocation(x, y);
+        if (house is not null)
+        {
+            if (!house.AllowedToInteract(Connection.ActiveChar))
+            {
+                Connection.ActiveChar.SendErrorMessage(ErrorMessageType.NoPerm);
                 return;
             }
 
-            Logger.Warn($"CreateDoodad, ItemId: {itemId}, FarmType: {farmType}");
+            laborCost = 0;
         }
 
+        // Check labor cost
+        if (Connection.ActiveChar.LaborPower < laborCost)
+        {
+            Connection.ActiveChar.SendErrorMessage(ErrorMessageType.LaborPowerNeeded);
+            return;
+        }
+
+        // Subtract labor
+        if (laborCost != 0)
+        {
+            Connection.ActiveChar.ChangeLabor((short)-laborCost, 0);
+        }
+
+        // Actually place the doodad
         DoodadManager.Instance.CreatePlayerDoodad(Connection.ActiveChar, id, x, y, z, zRot, scale, itemId, farmType);
     }
 }
