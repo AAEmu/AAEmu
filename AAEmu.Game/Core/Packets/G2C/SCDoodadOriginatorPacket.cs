8527caacaf7273abecf7863dd2a9c549008b2dc2
ZeromusXYZ <zeromus.xyz@gmail.com>
2026-07-19 11:43:00 +0200

    fix(game): Make sure all doodads get the new owner set on house sale (#1464)
    * fix(game): Make sure all doodads get the new owner set on house sale
    
    This fixes #871 [BUG] building plaque.
    
    When a house was sold, furniture owner information was not being fully updated. It only updated furniture place by the player and omitted the default house parts like doors and the building plaque.
    
    This included:
    - `UpdateFurnitureOwner` previously only updated furniture without a specific `AttachPointKind`, potentially missing other furniture types.
    - The `SCDoodadOriginatorPacket` sent a hardcoded `0` instead of the new owner's actual faction ID.
    
    This change ensures all furniture in a sold house has its owner ID updated, and clients receive the correct new owner's faction ID via the `SCDoodadOriginatorPacket`.
    
    * fix(game): Persist new owner for persistent furniture on house sale
    
    Previously, the updated owner ID for persistent furniture was not saved after a house sale,
    causing the owner to revert on server restart for such items. This ensures data integrity.

--- AAEmu.Game/Core/Packets/G2C/SCDoodadOriginatorPacket.cs
+++ AAEmu.Game/Core/Packets/G2C/SCDoodadOriginatorPacket.cs
@@ -1,16 +1,17 @@
 ﻿using AAEmu.Commons.Network;
 using AAEmu.Game.Core.Network.Game;
+using AAEmu.Game.Models.StaticValues;
 
 namespace AAEmu.Game.Core.Packets.G2C;
 
-public class SCDoodadOriginatorPacket(uint objId, uint newOwnerId, uint faction)
+public class SCDoodadOriginatorPacket(uint objId, uint newOwnerId, FactionsEnum newFaction)
     : GamePacket(SCOffsets.SCDoodadOriginatorPacket, 1)
 {
     public override PacketStream Write(PacketStream stream)
     {
         stream.WriteBc(objId);
         stream.Write(newOwnerId);
-        stream.Write(faction);
+        stream.Write((uint)newFaction);
 
         return stream;
     }
